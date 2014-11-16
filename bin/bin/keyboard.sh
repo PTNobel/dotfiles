47f8bf1b6d0c085d4de71165d95e801e0ca211bb
pnob <pnob99@gmail.com>
2014-11-16 14:23:50 -0800

    get it working.

--- bin/bin/keyboard.sh
+++ bin/bin/keyboard.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-VARIANT=`setxkbmap -query | awk '/layout/{print $2}'`
+VARIANT=`setxkbmap -query | awk '/variant/{print $2}'`
 
 if [ "$VARIANT" == "intl" ]
 
-then setxkbmap -layout us -variant
+then setxkbmap -layout us -variant ''
 
 else setxkbmap -layout us -variant intl #; echo " VARIANT = intl "
 
