0f913ee97d78d3feff0836becacd16ebbc0baecd
pnob <pnob99@gmail.com>
2014-11-23 15:42:10 -0800

    updates to the dirty hack.

--- bin/bin/transperancy.sh
+++ bin/bin/transperancy.sh
@@ -5,3 +5,6 @@ killall compton
 compton --config "$HOME/.i3/compton.transperancy.conf" &
 
 i3-msg '[ class=".*" ] border normal'
+
+# Quick and dirty hack until I figure out how to change new_window.
+bash -c 'while true ; do sleep 3 && i3-msg '[ class=".*" ] border normal' ; done' &
