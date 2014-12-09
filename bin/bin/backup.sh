6bcb75532c5a9d9eac0ab35c548521250dc7cebe
pnob <pnob99@gmail.com>
2014-12-08 22:19:05 -0800

    updates too backup.sh

--- bin/bin/backup.sh
+++ bin/bin/backup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 DESTINATION=/media/Toshiba_Backups
+list_files() {
+  \ls -a | grep -v \^$USER\$ | grep -v \^.\$ | grep -v \^..\$
+}
 if [ -d "$DESTINATION/" ]; then
-  #mv `\ls -a | grep -v \^$USER\$ | grep -v \^.\$ | grep -v \^..\$` $USER/
+  cd $DESTINATION
+  mkdir $USER
+  mv `list_files` $USER/ 
   rsync -aAv --delete --exclude=.cache $HOME $DESTINATION/
-  rsync -aAv --delete --exlude=.cache $HOME/.* $DESTINATION/$USER/
   rsync -aAv --delete $HOME/.dotfiles/* $DESTINATION/$USER/.dotfiles/
-  #mv $DESTINATION/$USER/* $DESTINATION
-  #mv $DESTINATION/$USER/.* $DESTINATION
-  #rmdir $DESTINATION/$USER
+  mv $DESTINATION/$USER/* $DESTINATION
+  cd $USER
+  mv `list_files` $DESTINATION
+  rmdir $DESTINATION/$USER
+  exit $?
 else echo $DESTINATION is not found ; exit 1
 fi
