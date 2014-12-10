66f5a3d61d4ec33f2f51128d951b9c4e62ceba33
pnob <pnob99@gmail.com>
2014-12-09 21:16:00 -0800

    updates and changes

--- bin/bin/update.sh
+++ bin/bin/update.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo dash -c 'echo "updating pkgfile database" && pkgfile -u && echo "updating mlocate database" && updatedb  && echo "updating man database" && mandb' | lolcat
+
+backup.sh ; sudo dash -c 'echo "updating pkgfile database" && pkgfile -u && echo "updating mlocate database" && updatedb  && echo "updating man database" && mandb' | lolcat
