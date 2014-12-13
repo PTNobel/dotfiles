4d9aaa5c2f53eed87cda74261b1d8e5bdb2d5ff1
pnob <pnob99@gmail.com>
2014-12-12 18:46:46 -0800

    finished rewrite Everything should work properly now

--- bin/bin/update.sh
+++ bin/bin/update.sh
@@ -7,10 +7,10 @@ export WATCHDOG_FILE=$(mktemp)
 #echo WATCHDOG_FILE=$WATCHDOG_FILE
 
 watchdog() {
-  until [ `cat $1 | wc -l` -eq 4 ];
+  until [ `cat $1 | wc -l` -eq $2 ];
     do sleep 10
   done
-  echo 4 processes have announced finishing: beginning shutdown routine
+  echo $2 processes have announced finishing: beginning shutdown routine
   echo deleting OUTPUT_FILE and WATCHDOG_FILE
   rm $OUTPUT_FILE $WATCHDOG_FILE
   echo killing script 
@@ -18,24 +18,49 @@ watchdog() {
   kill $$
 }
 
-watchdog $WATCHDOG_FILE &
+backup() {
+  backup.sh &>> $OUTPUT_FILE
+  echo DONE >> $WATCHDOG_FILE
+}
 
-sudo -v
+#TODO: Handle already locked mlocate.db
+mlocate() {
+  sudo -E updatedb &>> $OUTPUT_FILE
+  echo DONE >> $WATCHDOG_FILE
+}
 
+pkgfile_u() {
+  sudo -E pkgfile -u &>> $OUTPUT_FILE 
+  echo DONE >> $WATCHDOG_FILE
 
-echo "starting update of pkgfile database"
-sudo -E bash -c 'pkgfile -u &>> $OUTPUT_FILE ; echo DONE >> $WATCHDOG_FILE' &
+}
 
-echo "starting backup of $USER home directory"
-bash -c 'backup.sh &>> $OUTPUT_FILE ; echo DONE >> $WATCHDOG_FILE' &
+man_u() {
+  sudo -E mandb &>> $OUTPUT_FILE 
+  echo DONE >> $WATCHDOG_FILE
+}
 
-yaourt -Syua
+watchdog $WATCHDOG_FILE 5 &
 
+echo "starting backup of $HOME"
+backup &
+
+setsid yaourt -S &>/dev/null
 
 sudo -v
 
 echo "starting update of mlocate database"
-sudo -E bash -c 'updatedb &>> $OUTPUT_FILE ; echo DONE >> $WATCHDOG_FILE' &
+mlocate &
+
+echo "starting update of pkgfile database"
+pkgfile_u &
+
+yaourt -Syua
+
+echo "starting update of mlocate database"
+mlocate &
+
 echo "starting update of man database"
-sudo -E bash -c 'mandb &>> $OUTPUT_FILE ; echo DONE >> $WATCHDOG_FILE' &
-tail -f $OUTPUT_FILE | lolcat
+man_u &
+
+tail -n`cat $OUTPUT_FILE | wc -l`  -f $OUTPUT_FILE | lolcat
