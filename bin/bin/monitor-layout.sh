d9279bbf447b6ba6a10cb69065938e0f14046087
pnob <pnob99@gmail.com>
2015-01-03 14:56:07 -0800

    it's actually readable now!

--- bin/bin/monitor-layout.sh
+++ bin/bin/monitor-layout.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-xrandr --output HDMI1 --mode 1920x1080 --pos 1366x0 --rotate normal --primary
-if [ $? -eq 1 ]; then xrandr --output LVDS1 --mode 1366x768 --pos 0x809 --rotate normal --primary ; xrandr --output HDMI1 --off
+if xrandr --output HDMI1 --mode 1920x1080 --pos 1366x0 --rotate normal --primary; then
+    xrandr --output LVDS1 --mode 1366x768 --pos 0x809 --rotate normal
 else
-xrandr --output LVDS1 --mode 1366x768 --pos 0x809 --rotate normal
+    xrandr --output LVDS1 --mode 1366x768 --pos 0x0 --rotate normal --primary ; xrandr --output HDMI1 --off
 fi
 xrandr --output VIRTUAL1 --off
 xrandr --output DP1 --off
 xrandr --output VGA1 --off
+xrandr --setprovideroffloadsink radeon Intel
