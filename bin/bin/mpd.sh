00d908ad096ff9566c3d49e2b642db8b3ba3ea1b
pnob <pnob99@gmail.com>
2015-01-04 15:12:12 -0800

    spelling error

--- bin/bin/mpd.sh
+++ bin/bin/mpd.sh
@@ -4,7 +4,7 @@ killall mpd &>/dev/null
 
 #sleep 1
 
-mpd ~/.config/mpd/mpd.conf || mpd ~/.confg/mpd/mpd.conf.offline
+mpd ~/.config/mpd/mpd.conf || mpd ~/.config/mpd/mpd.conf.offline
 if [ $? -eq 0 ]; then
 mpd #~/.config/mpd/mpd.conf.online
 else mpd ~/.config/mpd/mpd.conf
