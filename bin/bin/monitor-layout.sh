b9bd52741f5bb184c46d38867bf70dc346f716f8
pnob <pnob99@gmail.com>
2015-01-11 11:43:01 -0800

    complete rewrite, now runs in the background.

--- bin/bin/monitor-layout.sh
+++ bin/bin/monitor-layout.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+'#TODO xrandr -q
 if xrandr --output HDMI1 --mode 1920x1080 --pos 1366x0 --rotate normal --primary; then
     xrandr --output LVDS1 --mode 1366x768 --pos 0x809 --rotate normal
 else
@@ -7,4 +9,51 @@ fi
 xrandr --output VIRTUAL1 --off
 xrandr --output DP1 --off
 xrandr --output VGA1 --off
+xrandr --setprovideroffloadsink radeon Intel'
+#!/bin/bash
+
+kill `pidof -x $0 -o %PPID`
+
+# default monitor is LVDS1
+MONITOR=LVDS1
+
+# functions to switch from LVDS1 to HDMI and vice versa
+function ActivateHDMI {
+    echo "Switching to HDMI1"
+    xrandr --output HDMI1 --mode 1920x1080 --pos 1366x0 --rotate normal --primary
+	xrandr --output LVDS1 --mode 1366x768  --pos 0x809  --rotate normal
+    MONITOR=HDMI1
+}
+function DeactivateHDMI {
+    echo "Switching to LVDS1"
+    xrandr --output HDMI1 --off
+	xrandr --output LVDS1 --mode 1366-768  --pos 0x0    --rotate normal --primary 
+    MONITOR=LVDS1
+}
+
+# functions to check if HDMI is connected and in use
+function HDMIActive {
+    [ $MONITOR = "HDMI1" ]
+}
+function HDMIConnected {
+    ! xrandr | grep "^HDMI1" | grep disconnected
+}
+
+xrandr --output VIRTUAL1 --off
+xrandr --output DP1 --off
+xrandr --output VGA1 --off
 xrandr --setprovideroffloadsink radeon Intel
+
+# actual script
+while true; do
+
+    if ! HDMIActive && HDMIConnected; then
+        ActivateHDMI
+
+    elif HDMIActive && ! HDMIConnected; then
+        DeactivateHDMI
+
+    fi
+
+    sleep 1s
+done
