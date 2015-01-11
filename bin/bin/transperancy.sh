ff1d504791472e01e529bab0ac0e66f8187ad32c
pnob <pnob99@gmail.com>
2015-01-10 18:45:07 -0800

    complete restructuring of transperancy.sh
    Now it switches window decorations based on the trasperancy level it
    also switches transperancy on and off now

--- bin/bin/transperancy.sh
+++ bin/bin/transperancy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if cat /proc/$(pgrep compton)/cmdline | grep transperancy ; then
+  killall compton
+  compton --config "$HOME/.i3/compton.conf" &
+  i3-msg '[ class=".*" ] border pixel'
+else
 killall compton
 
 compton --config "$HOME/.i3/compton.transperancy.conf" &
@@ -7,4 +12,9 @@ compton --config "$HOME/.i3/compton.transperancy.conf" &
 i3-msg '[ class=".*" ] border normal'
 
 # Quick and dirty hack until I figure out how to change new_window.
-#bash -c "while true ; do sleep 1 && i3-msg '[ class=\".*\" ] border normal &>/dev/null' ; done" &
+fi
+for i in $(pgrep bash); do
+	cd /proc/$i 
+	cat cmdline | grep windowdecorations.sh && kill $i
+done
+bash -c "while true ; do sleep 1 && windowdecorations.sh &>/dev/null ; done" &
---
+++ bin/bin/windowdecorations.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if cat /proc/$(pgrep compton)/cmdline | grep transperancy ; then
+  i3-msg '[ class=".*" ] border normal'
+else 
+  echo nothing to do
+fi
