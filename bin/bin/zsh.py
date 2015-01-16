2dabaad67680d2ccbc53c68ab33488e2b3b2dcd9
pnob <pnob99@gmail.com>
2015-01-16 15:45:35 -0800

    Makes the *sh.py and python.py pylint clean

--- bin/bin/zsh.py
+++ bin/bin/zsh.py
@@ -2,32 +2,32 @@
 # -*- coding: utf-8 -*-
 #
 #  zsh.py
-#  
+#
 #  Copyright 2014 Parth Nobel <pnob99@gmail.com>
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
-#  
+#
+#
 
 import os
-exit_code=False
+exit_code = False
 def zsh():
-     return os.system("bash -c 'echo -ne \"\033c\" && zsh'")
+    return os.system("bash -c 'echo -ne \"\033c\" && zsh'")
 while exit_code != 9472 and exit_code != 10752:
     os.system('echo -ne "\033c"')
-    exit_code=zsh()
+    exit_code = zsh()
 os.system('echo -ne "\033c"')
 exit(0)
