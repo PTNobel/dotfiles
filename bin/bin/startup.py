b8be40058e88da8d5d550cdf575da4d38fc1694d
pnob <pnob99@gmail.com>
2015-01-16 18:42:24 -0800

    rewrite adds weekly runs, cleans all the code, almost pylint clean.

--- bin/bin/startup.py
+++ bin/bin/startup.py
@@ -3,69 +3,66 @@
 import os
 import sys
 import datetime
-import subprocess
 
-log="/home/parth/.parth/date.log"
-autostart="/home/parth/.i3/autostart"
-bootstrap="/home/parth/.i3/bootstrap"
-log_file=open(log, 'r')
-today=str(datetime.date.today())
-autostart_file_list=open(autostart,'r')
-bootstrap_file_list=open(bootstrap,'r')
-commands=list()
-bootstrap_commands=list()
+date_log = "/home/parth/.parth/date.log"
+autostart = "/home/parth/.i3/autostart"
+bootstrap = "/home/parth/.i3/bootstrap"
+week = "/home/parth/.i3/weekly_tasks"
+date_log_file = open(date_log, 'r')
+log_value = date_log_file.read()
+date_log_file.close()
+today = str(datetime.date.today())
+autostart_file_list = open(autostart, 'r')
+bootstrap_file_list = open(bootstrap, 'r')
+weekly_file_list = open(week, 'r')
+command_list = list()
+bootstrap_commands = list()
+weekly = list()
 
-raw_commands=autostart_file_list.readlines() + bootstrap_file_list.readlines()
-raw_bootstarp_commands=bootstrap_file_list.readlines()
+raw_commands = autostart_file_list.readlines()
+raw_bootstarp_commands = bootstrap_file_list.readlines()
+raw_weekly = weekly_file_list.readlines()
+raw_commands += raw_bootstarp_commands
 
 for i in raw_commands:
-    var=i.replace('\n','')
-    commands.append(var)
+    var = i.replace('\n', '')
+    command_list.append(var)
 for i in raw_bootstarp_commands:
-    var=i.replace('\n','')
+    var = i.replace('\n', '')
     bootstrap_commands.append(var)
+for i in raw_weekly:
+    var = i.replace('\n', '')
+    weekly.append(var)
 
 #print(commands)
-def update_log(log_file,log):
-    log_file.close()
-    log_file_writeable=open(log,'w')
+def update_log(log):
+    log_file_writeable = open(log, 'w')
     log_file_writeable.write(today)
     log_file_writeable.close()
-    
+
 def success(commands):
     print("starting first boot routine")
     for i in commands:
         os.system(i + " &")
-        #os.spawnl(os.P_NOWAIT,i)
-"""    for i in commands:
-        #os.system("setsid " + str(i))
-        call_command = ["setsid"]
-        print(commands)
-        print(i)
-        for j in commands:
-            var=j.split(' ')
-            print(j)
-            for l in var:
-                call_command.append(l)
-                print(call_command)
-                print(subprocess.call(call_command))
-"""
+        #os.spawnl(os.P_NOWAIT, i)
 #print(commands)
 if len(sys.argv) > 1:
     if sys.argv[1] == "-f":
-       success(commands)
+        success(command_list)
+    elif sys.argv[1] == "-b":
+        success(bootstrap_commands)
 
-elif log_file.read() == today:
-   log_file.close()
-   print("The computer has already booted today")
-   if len(sys.argv) > 1:
-       if sys.argv[1] == "-b":
-           success(bootstrap_commands)
-elif log_file.read() != today and os.system('xrandr | grep HDMI1 | grep connected') == 1:
+elif os.system('xrandr | grep HDMI1 | grep disconnected >/dev/null') == 0:
     success(bootstrap_commands)
 
+elif log_value != today:
+    if datetime.date.today().weekday() == 4:
+        command_list += weekly
+    update_log(date_log)
+    success(command_list)
+
 else:
-    update_log(log_file,log)
-    success(commands)
+    success(bootstrap_commands)
+
 
 exit(0)
