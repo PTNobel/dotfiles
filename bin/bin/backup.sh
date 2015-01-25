f8b5e22ee451671618d256e3347e56b03e2924ab
pnob <pnob99@gmail.com>
2015-01-24 22:21:07 -0800

    utter rewrite of backup.sh about a dozen times safer.

--- bin/bin/backup.sh
+++ bin/bin/backup.sh
@@ -1,39 +1,48 @@
 #!/bin/bash
 
+set -e
+set -u
+
 #DEFINTIONS!
 PRIMARY_DIRECTORY=/media/Toshiba_Backups
 DESTINATION=${PRIMARY_DIRECTORY}/Backups
 DUPLICITY_DESTINATION=file://${PRIMARY_DIRECTORY}/Duplicity
 list_files() {
-  \ls -a | grep -v \^$USER\$ | grep -v \^.\$ | grep -v \^..\$
+		VAR=$(/bin/ls -a | grep -v \^$USER\$ | grep -v \^.\$ | grep -v \^..\$) #| sed s/\ /\\\ /g
+  ESCAPE_KEY='\ '
+  echo ${VAR// /$ESCAPE_KEY}
 }
 
 #Check user
 
 if [ -d "$PRIMARY_DIRECTORY/" ]; then
-  if  [ -f $DESTINATION/$USER ]; then
+  if [ -f $DESTINATION/$USER ]; then
 
 # No files/directories should be hardcoded
     chmod -R +w $DESTINATION
-    cd $DESTINATION
-    rm $DESTINATION/$USER
+    #cd $DESTINATION
+    #rm $DESTINATION/$USER
     #ls #debug
     #file $USER  #debug
     #sleep 30 #debug
-    mkdir $USER
-    mv `list_files` $USER/ 
-    rsync -aAv --delete --exclude=.cache $HOME $DESTINATION/
-    mv $DESTINATION/$USER/* $DESTINATION
-    cd $USER
-    mv `list_files` $DESTINATION
-    rmdir $DESTINATION/$USER
+    #mkdir $DESTINATION/$USER
+	#mv $(list_files) $DESTINATION/$USER/ 
+	echo pwd=$PWD
+	ls
+	mv $DESTINATION $PRIMARY_DIRECTORY/$USER
+    bash -c "rsync -apv --delete --exclude=.cache $HOME $PRIMARY_DIRECTORY ; true"
+    #mv $DESTINATION/$USER/* $DESTINATION
+    mv $PRIMARY_DIRECTORY/$USER $DESTINATION
+    #cd $USER
+	#mv $(list_files) $DESTINATION
+    #rmdir $DESTINATION/$USER
     #sleep 40 #debug
     echo $USER > $DESTINATION/$USER
     chmod -R a-w $DESTINATION
-    exit $?
+    #exit $?
   else echo $DESTINATION not authorized for write by current user ; exit 2
   fi
-  echo starting duplicity
+  echo starting duplicity ; notify-send duplicity started
   duplicity incremental --no-encryption $HOME $DUPLICITY_DESTINATION 
 else echo $DESTINATION is not found ; exit 1
 fi
