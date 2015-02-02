f1c2da0e9bd8332907c09d7aa6854d8a80d56c60
pnob <pnob99@gmail.com>
2015-02-01 20:41:17 -0800

    removes TABS in monitor-layout.sh

--- bin/bin/monitor-layout.sh
+++ bin/bin/monitor-layout.sh
@@ -2,7 +2,7 @@
 
 #TODO xrandr -q
 if ! xrandr | grep "^HDMI" | grep disconnected; then
-	xrandr --output HDMI1 --mode 1920x1080 --pos 1366x0 --rotate normal --primary
+    xrandr --output HDMI1 --mode 1920x1080 --pos 1366x0 --rotate normal --primary
     xrandr --output LVDS1 --mode 1366x768 --pos 0x809 --rotate normal
 else
     xrandr --output LVDS1 --mode 1366x768 --pos 0x0 --rotate normal --primary ; xrandr --output HDMI1 --off
@@ -20,16 +20,16 @@ kill `pidof -x $0 -o %PPID`
 function ActivateHDMI {
     echo "Switching to HDMI1"
     xrandr --output HDMI1 --mode 1920x1080 --pos 1366x0 --rotate normal --primary
-	xrandr --output LVDS1 --mode 1366x768  --pos 0x809  --rotate normal
-	sh $HOME/.fehbg
+    xrandr --output LVDS1 --mode 1366x768  --pos 0x809  --rotate normal
+    sh $HOME/.fehbg
     MONITOR="HDMI1"
 }
 function DeactivateHDMI {
     echo "Switching to LVDS1"
     xrandr --output HDMI1 --off
-	xrandr --output LVDS1 --mode 1366-768  --pos 0x0    --rotate normal --primary 
+    xrandr --output LVDS1 --mode 1366-768  --pos 0x0    --rotate normal --primary 
     MONITOR="LVDS1"
-	sh $HOME/.fehbg
+    sh $HOME/.fehbg
 }
 
 # functions to check if HDMI is connected and in use
