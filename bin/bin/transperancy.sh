03adbdbc3c323ece9372e6ebd9f831ee266a2c0c
pnob <pnob99@gmail.com>
2015-02-01 20:42:52 -0800

    fixes tabs in transperacy.sh

--- bin/bin/transperancy.sh
+++ bin/bin/transperancy.sh
@@ -14,7 +14,7 @@ i3-msg '[ class=".*" ] border normal'
 # Quick and dirty hack until I figure out how to change new_window.
 fi
 for i in $(pgrep bash); do
-	cd /proc/$i 
-	cat cmdline | grep windowdecorations.sh && kill $i
+    cd /proc/$i 
+    cat cmdline | grep windowdecorations.sh && kill $i
 done
 bash -c "while true ; do sleep 1 && windowdecorations.sh &>/dev/null ; done" &
