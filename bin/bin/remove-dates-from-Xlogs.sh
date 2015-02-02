c6586d5e19c257a4fcc26b74d11f39b4a40bf11d
pnob <pnob99@gmail.com>
2015-02-01 21:03:26 -0800

    documents it and makes cleans up the code.

--- bin/bin/remove-dates-from-Xlogs.sh
+++ bin/bin/remove-dates-from-Xlogs.sh
@@ -1,3 +1,17 @@
 #!/bin/zsh
+# 
+# So to use this script, take a copy of Xorg.#.log (where # is a number)
+# and open it in vim, possibly vi too (I haven't tested it in vi)
+# and then launch this script in a different terminal, move the mouse 
+# over the terminal with vim and watch it work. Consider getting some tea.
+#
+#
 
-sleep 3 ; while sleep .3; cat Xorg.0.log.old.2 | grep "^\[" >|/dev/null ; do xdotool key n ;  xdotool key 5 d w ; xdotool key colon w Return ; done ; notify-send Done\ with\ this\ file
+
+sleep 3
+while sleep .3; cat "$1" | grep "^\[" >|/dev/null ; do
+    xdotool key slash asciicircum backslash bracketleft Return # /^\[
+    xdotool key 5 d w
+    xdotool key colon w Return
+done
+notify-send Done\ with\ this\ file
