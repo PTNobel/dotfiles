e41d7454fbce6d1672ce51188296682138a97200
pnob <pnob99@gmail.com>
2015-02-07 22:29:28 -0800

    updates to remove_dates-from-Xlogs.sh, It's still broken

--- bin/bin/remove-dates-from-Xlogs.sh
+++ bin/bin/remove-dates-from-Xlogs.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
-# 
+#
+# Usage: $0 Xorg.*.log
 # So to use this script, take a copy of Xorg.#.log (where # is a number)
 # and open it in vim, possibly vi too (I haven't tested it in vi)
 # and then launch this script in a different terminal, move the mouse 
@@ -7,11 +8,21 @@
 #
 #
 
+export WINDOW_LOG_FILE=$(mktemp)
 
-sleep 3
+echo xterm -e zsh -c "echo \$WINDOWID >| $WINDOW_LOG_FILE ; vim $1" &
+xterm -e zsh -c "echo \$WINDOWID >| $WINDOW_LOG_FILE ; vim $1" &
+echo $WINDOW_LOG_FILE; cat $WINDOW_LOG_FILE
+sleep 5
+export WINDOW_ID_OF_VIM=$(cat $WINDOW_LOG_FILE)
+echo $WINDOW_LOG_FILE; cat $WINDOW_LOG_FILE
+echo $WINDOW_ID_OF_VIM
 while sleep .3; cat "$1" | grep "^\[" >|/dev/null ; do
-    xdotool key slash asciicircum backslash bracketleft Return # /^\[
-    xdotool key 5 d w
-    xdotool key colon w Return
+    echo starting a round
+    xdotool key --window $WINDOW_ID_OF_VIM slash asciicircum backslash bracketleft Return # /^\[
+    xdotool key --window $WINDOW_ID_OF_VIM 5 d w
+    xdotool key --window $WINDOW_ID_OF_VIM colon w Return
+    exit 1
 done
+rm $WINDOW_LOG_FILE
 notify-send Done\ with\ this\ file
