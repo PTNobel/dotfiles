bca421aeea0140f2c6cc54e3e7c404589bd4fa0d
pnob <pnob99@gmail.com>
2015-02-12 22:07:34 -0800

    Creates a recursive function in pianoctl.py

--- bin/bin/pianoctl.py
+++ bin/bin/pianoctl.py
@@ -2,12 +2,23 @@
 
 import os
 
+def prompt_user(secret_num=0):
+    if secret_num >= 5:
+        exit(1)
+    else:
+        try:
+            user_input = input('Do you want to launch pianoctl? ').lower()
+        except EOFError:
+            exit(0)
+        if user_input == 'y' or user_input == "yes":
+            os.system('pianoctl')
+        elif user_input == 'n' or user_input == "no" or user_input == "\n":
+            exit(1)
+        else:
+            prompt_user(secret_num+1)
+
 if os.system('pidof pianobar >/dev/null') == 0:
     os.system('pianoctl')
 
 else:
-    user_input = input('Do you want to launch pianoctl? ').lower()
-    if user_input == 'y' or user_input == "yes":
-        os.system('pianoctl')
-    elif user_input == 'n' or user_input == "no":
-        exit(1)
+    prompt_user()
