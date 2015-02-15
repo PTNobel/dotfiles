985f88072ecf946762140c419dfdd2f87576a241
pnob <pnob99@gmail.com>
2015-02-14 20:56:22 -0800

    updates dunst.

--- bin/bin/dunst.sh
+++ bin/bin/dunst.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
 setsid dunst -config ~/.i3/dunstrc 2>&1 | grep "Unknown keyboard shortcut: mod4+dead_grave"
-if [ $? -eq 0 ] ; then
-    echo if statement
-    killall dunst
-    setsid dunst -config ~/.i3/grave.dunstrc | grep Unknown 
-fi
