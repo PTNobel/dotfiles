1d257275a8ef06bb3ea7d643e1df37a783c261b2
pnob <pnob99@gmail.com>
2015-02-15 17:08:05 -0800

    Improves install.sh, it now behaves more like make.

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ stow_wrapper(){
     for i in $(cat "$MANIFEST_FILE"); do
         if [ -d $i ] || true ; then
             #echo $i $1
+            echo stow $1 "$i"
             stow $1 "$i"
         else 
             echo "$i" is not valid content for $ROOT_DIR/manifest.repos
@@ -27,6 +28,7 @@ stow_wrapper -D
 for i in $(cat $(pwd -P)/manifest_build.repos); do
     cd $i
     make
+    make test
     sudo make install
     make clean
     cd $(dirname $0) 
