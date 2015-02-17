53791660a0cf3e6dd7fe4ef045caf4d5cd5432a5
pnob <pnob99@gmail.com>
2015-02-16 20:57:34 -0800

    Allows $? of tests.sh to specify reason of failure.

--- update.sh_helper/tests.sh
+++ update.sh_helper/tests.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# If $? at the end is 1 then the build failed.
+# If $? at the end is 2 then one of the binaries referenced doesn't exist.
+#
 
 [ -x update_tools_helper ] || exit 1
 
@@ -12,4 +16,4 @@ for i in $(strings update_tools_helper | grep bin); do
 done
 }
 
-_test || exit 1
+_test || exit 2
