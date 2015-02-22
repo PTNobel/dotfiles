3264efce9b924995ac7718f6287511e8cda110f9
pnob <pnob99@gmail.com>
2015-02-22 14:32:48 -0800

    Adds some debug ouput.

--- update.sh_helper/tests.sh
+++ update.sh_helper/tests.sh
@@ -4,6 +4,7 @@
 # If $? at the end is 2 then one of the binaries referenced doesn't exist.
 #
 
+[ -x update_tools_helper ] || echo Where is update_tools_helper?
 [ -x update_tools_helper ] || exit 1
 
 _test(){
@@ -11,6 +12,7 @@ for i in $(strings update_tools_helper | grep bin); do
     if echo $i | grep \- >/dev/null; then
         true
     else
+        [ -x $i ] || echo can\'t find $i.
         [ -x $i ] || exit 1
     fi
 done
