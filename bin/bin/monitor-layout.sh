b25d88c7493500141d29725032efc3b69247b057
pnob <pnob99@gmail.com>
2015-02-27 19:18:08 -0800

    Removes TODO statement which was already finished.

--- bin/bin/monitor-layout.sh
+++ bin/bin/monitor-layout.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#TODO xrandr -q
 if ! xrandr | grep "^HDMI" | grep disconnected; then
     xrandr --output HDMI1 --mode 1920x1080 --pos 1366x0 --rotate normal --primary
     xrandr --output LVDS1 --mode 1366x768 --pos 0x809 --rotate normal
