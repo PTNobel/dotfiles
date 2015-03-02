5266f48196329ad9ac930d584463fb54487b18e5
pnob <pnob99@gmail.com>
2015-03-01 18:22:46 -0800

    moves touchpad.sh to my coding style.

--- bin/bin/touchpad.sh
+++ bin/bin/touchpad.sh
@@ -5,8 +5,7 @@ eval "$(synclient | grep TouchpadOff | sed s/\ //g)"
 #ID=`xinput list | grep -Eo 'TouchPad\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}'`
 #declare -i STATE
 #STATE=`xinput list-props $ID|grep 'Device Enabled'|awk '{print $4}'`
-if [ $TouchpadOff -eq 1 ]
-then
+if [ $TouchpadOff -eq 1 ] ; then
    synclient TouchpadOff=0
    echo Touchpad Enabled
 else
