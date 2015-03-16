090c9687102cabe02f3219df9b5fe9fd611366fb
pnob <pnob99@gmail.com>
2015-03-15 17:50:41 -0700

    Expands and documents external API of musicctl.sh

--- bin/bin/musicctl.sh
+++ bin/bin/musicctl.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# 
+# This code is poorly written just an FYI.
+# It should work. However musicctl.py is much better.
+# This code is probably insecure if someone really tries to exploit it.
+# So don't use this in any situation where the user doesn't already have
+# shell access.
 
 # Pianobar or mpd?
 get_player() {
@@ -58,4 +64,31 @@ self.stop() {
    fi
 }
 
-self."$1"
+self.quit() {
+   self.stop
+}
+
+self.usage() {
+   printf "Usage:
+   $0 pause
+   $0 play
+   $0 back 
+   $0 next
+   $0 stop
+   $0 quit\n"
+}
+
+self.help() {
+   self.usage
+}
+
+self.-h() {
+   self.usage
+}
+
+self.() {
+   self.usage
+   exit 1
+}
+
+self."$1" || exit 1
