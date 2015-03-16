bd5a2a5b04d00009eca65ba3550f5903a63eaf00
pnob <pnob99@gmail.com>
2015-03-15 17:53:21 -0700

    Cleans pianoctl.py and lays groundwork for a new way to test for pianobar

--- bin/bin/pianoctl.py
+++ bin/bin/pianoctl.py
@@ -2,12 +2,14 @@
 
 import os
 
+
 def prompt_user(secret_num=0):
     if secret_num >= 5:
         exit(1)
     else:
         try:
-            user_input = input('Do you want to launch pianoctl? ([Y]es|[N]o) ').lower()
+            user_input = input('Do you want to launch pianoctl? ([Y]es|[N]o) '
+                               ).lower()
         except EOFError:
             exit(0)
         if user_input == 'y' or user_input == "yes":
@@ -19,6 +21,12 @@ def prompt_user(secret_num=0):
 
 if os.system('pidof pianobar >/dev/null') == 0:
     os.system('pianoctl')
+
+elif True:
+    prompt_user()
+
+elif 'ctl' in os.listdir(os.environ['HOME'] + '/.config/pianobar/'):
+    None
 
 else:
     prompt_user()
