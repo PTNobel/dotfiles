26e631aa54ad80a609b970076195c62d7dcaa332
pnob <pnob99@gmail.com>
2015-03-16 18:06:15 -0700

    Finishes the rewrite of musicctl.py

--- bin/bin/musicctl.py
+++ bin/bin/musicctl.py
@@ -6,8 +6,8 @@ import os
 import sys
 
 
-def warning(*objs):
-    printed_list = 'WARNING: '
+def warning(*objs, prefix='WARNING: '):
+    printed_list = str(prefix)
     for i in objs:
         printed_list += i
     print(printed_list, file=sys.stderr)
@@ -16,27 +16,20 @@ def warning(*objs):
 def usage(exit_code, name_of_program):
     usage_text = "Usage: " + name_of_program + \
         " {play|pause|back|stop|next|toggle|usage|help}"
-
     if exit_code == 0:
         print(usage_text)
     elif exit_code > 0:
-        warning(usage_text)
+        warning(usage_text, prefix='')
     else:
         exit_code = 1
     exit(exit_code)
 
 
 def processargs(argv):
-    output = {"verbose": None, "input": None, "name": None}
+    output = {"verbose": None, "input": None}
     output["name"] = argv[0]
-    for i in argv:
-        if i == "-h":
-            usage(0, output["name"])
-        if i == "-v" or i == "--verbose":
-            output["verbose"] = True
     if len(argv) == 1:
-        output = None
-        warning("Not enough arguements")
+        warning("Not enough arguments")
         usage(1, output["name"])
     elif len(argv) >= 2:
         if argv[-1] == "help" or argv[-1] == "usage":
@@ -44,11 +37,16 @@ def processargs(argv):
 
         else:
             output["input"] = argv[-1]
+    for i in argv:
+        if i == "-h":
+            usage(0, output["name"])
+        if i == "-v" or i == "--verbose":
+            output["verbose"] = True
     return output
 
-arguements = processargs(sys.argv)
+arguments = processargs(sys.argv)
 
-if arguements["verbose"]:
+if arguments["verbose"]:
     def verboseprint(*args):
         # Print each argument separately so caller doesn't need to
         # stuff everything to be printed into a single string
@@ -61,20 +59,24 @@ else:
 
 class musicctl:
 
-    def __init__(self, arguements):
+    def __init__(self, program_arguments):
         self.get_player()
-        self.program_name = arguements["name"]
-        self.operation = arguements["input"]
+        self.program_name = program_arguments["name"]
+        self.operation = program_arguments["input"]
+        verboseprint(self.player)
+        self.make_command_dict()
+        self.communicate_with_player()
+
+    def make_command_dict(self):
         self.pianobar_dict = {'play': "pianoctl p", 'pause': "pianoctl p",
                               'back': "pianoctl +", 'next': "pianoctl -",
-                              'quit': "pianoctl q", 'stop': "pianoctl q",
+                              'quit': "pianoctl q", 'stop': "pianoctl q ; \
+                              sleep 3 ; pidof pianobar && killall pianobar",
                               'tired': "pianoctl t"}
         self.mpd_dict = {'play': "mpc toggle", 'pause': "mpc toggle",
                          'back': "mpc prev", 'next': "mpc next",
                          'quit': "mpc stop", 'stop': "mpc stop"}
         self.commands = {'pianobar': self.pianobar_dict, 'mpd': self.mpd_dict}
-        verboseprint(self.player)
-        self.communicate_with_player()
 
     def get_player(self):
         if os.system("pidof mpd >/dev/null") == 0:
@@ -100,4 +102,4 @@ class musicctl:
             warning("Invalid input")
             usage(1, self.program_name)
 
-music = musicctl(arguements)
+music = musicctl(arguments)
