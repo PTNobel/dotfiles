30305507b79d43851b9a70346117f672c1dc8f5f
pnob <pnob99@gmail.com>
2015-03-17 19:44:21 -0700

    Refactors musicctl.py.

--- bin/bin/musicctl.py
+++ bin/bin/musicctl.py
@@ -25,28 +25,33 @@ def usage(exit_code, name_of_program):
     exit(exit_code)
 
 
-def processargs(argv):
-    output = {"verbose": None, "input": None}
-    output["name"] = argv[0]
-    if len(argv) == 1:
-        warning("Not enough arguments")
-        usage(1, output["name"])
-    elif len(argv) >= 2:
-        if argv[-1] == "help" or argv[-1] == "usage":
-            usage(0, output["name"])
-
+def processargs(argv, verbose_check=False):
+    if verbose_check:
+        if '-v' in argv or '--verbose' in argv:
+            output = True
         else:
-            output["input"] = argv[-1]
-    for i in argv:
-        if i == "-h":
-            usage(0, output["name"])
-        if i == "-v" or i == "--verbose":
-            output["verbose"] = True
+            output = False
+    else:
+        output = {"verbose": None, "input": None}
+        output["name"] = argv[0]
+        if len(argv) == 1:
+            warning("Not enough arguments")
+            usage(1, output["name"])
+        elif len(argv) >= 2:
+            if argv[-1] == "help" or argv[-1] == "usage":
+                usage(0, output["name"])
+
+            else:
+                output["input"] = argv[-1]
+        for i in argv:
+            if i == "-h":
+                usage(0, output["name"])
+            if i == "-v" or i == "--verbose":
+                output["verbose"] = True
     return output
 
-arguments = processargs(sys.argv)
 
-if arguments["verbose"]:
+if processargs(sys.argv, verbose_check=True):
     def verboseprint(*args):
         # Print each argument separately so caller doesn't need to
         # stuff everything to be printed into a single string
@@ -57,54 +62,43 @@ else:
         return
 
 
-class musicctl:
-
-    def __init__(self, program_arguments):
-        self.get_player()
-        self.program_name = program_arguments["name"]
-        self.operation = program_arguments["input"]
-        verboseprint(self.player)
-        self.make_command_dict()
-        self.communicate_with_player()
-
-    def make_command_dict(self):
-        self.pianobar_dict = {'play': "pianoctl p", 'pause': "pianoctl p",
-                              'back': "pianoctl +", 'next': "pianoctl -",
-                              'quit': "pianoctl q", 'stop': "pianoctl q ; \
-                              sleep 3 ; pidof pianobar && killall pianobar",
-                              'tired': "pianoctl t"}
-        self.mpd_dict = {'play': "mpc toggle", 'pause': "mpc toggle",
-                         'back': "mpc prev", 'next': "mpc next",
-                         'quit': "mpc stop", 'stop': "mpc stop"}
-        self.commands = {'pianobar': self.pianobar_dict, 'mpd': self.mpd_dict}
-
-    def get_player(self):
-        if os.system("pidof mpd >/dev/null") == 0:
-            if os.system("pidof pianobar >/dev/null") == 0:
-                if os.system("mpc status | grep playing &>/dev/null") == 0:
-                    self.player = "mpd"
-                else:
-                    self.player = "pianobar"
+def main(raw_argv):
+    arguments = processargs(raw_argv)
+    # TODO replace get_player()
+    if os.system("pidof mpd >/dev/null") == 0:
+        if os.system("pidof pianobar >/dev/null") == 0:
+            if os.system("mpc status | grep playing &>/dev/null") == 0:
+                player = "mpd"
             else:
-                self.player = "mpd"
-        elif os.system("pidof pianobar >/dev/null") == 0:
-            self.player = "pianobar"
+                player = "pianobar"
         else:
-            warning("No music player found")
-            usage(-1)
+            player = "mpd"
+    elif os.system("pidof pianobar >/dev/null") == 0:
+        player = "pianobar"
+    else:
+        warning("No music player found")
+        usage(-1)
 
-    def communicate_with_player(self):
-        try:
-            verboseprint('In communicate_with_player')
-            verboseprint(self.commands[self.player][self.operation])
-            os.system(self.commands[self.player][self.operation])
-        except KeyError:
-            warning("Invalid input")
-            usage(1, self.program_name)
+    program_name = arguments["name"]
+    operation = arguments["input"]
+    verboseprint(player)
+    pianobar_dict = {'play': "pianoctl p", 'pause': "pianoctl p",
+                     'back': "pianoctl +", 'next': "pianoctl -",
+                     'quit': "pianoctl q", 'stop': "pianoctl q ; " +
+                     "sleep 3 ; pidof pianobar && killall pianobar",
+                     'tired': "pianoctl t"}
+    mpd_dict = {'play': "mpc toggle", 'pause': "mpc toggle",
+                'back': "mpc prev", 'next': "mpc next",
+                'quit': "mpc stop", 'stop': "mpc stop"}
+    commands = {'pianobar': pianobar_dict, 'mpd': mpd_dict}
+    verboseprint(commands)
+    try:
+        verboseprint(commands[player][operation])
+        os.system(commands[player][operation])
+    except KeyError:
+        warning("Invalid input")
+        usage(1, program_name)
 
-
-def main():
-    musicctl(arguments)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
