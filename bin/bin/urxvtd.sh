ad3ac14ccd22ed9492c3ed2b8f3adadac5c11ca8
pnob <pnob99@gmail.com>
2015-03-22 14:59:42 -0700

    Tries to fix issue with multiple urxvtd.sh

--- bin/bin/urxvtd.sh
+++ bin/bin/urxvtd.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-while true
-do if [ `pidof urxvtd | wc -w` -lt 1 ]
-then urxvtd
-else sleep 10
+for i in $(pidof -x firefox.sh -o %PPID) ; do
+    if strings /proc/$i/environ | grep DISPLAY | grep ${DISPLAY} >/dev/null ; then
+        kill $i
+    fi
+done
+
+while true ; do
+    if [ `pidof urxvtd | wc -w` -lt 1 ]; then
+        urxvtd
+else
+    sleep 10
 fi
 done
