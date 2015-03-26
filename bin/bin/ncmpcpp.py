49f730245f406c6cb2c9f9573b846e9c13bcfdc3
pnob <pnob99@gmail.com>
2015-03-25 17:26:24 -0700

    Adds a pianoctl.py fork for ncmpcpp

---
+++ bin/bin/ncmpcpp.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import os
+
+
+def prompt_user(secret_num=0):
+    if secret_num >= 5:
+        exit(1)
+    else:
+        try:
+            user_input = input('Do you want to launch mpd? ([Y]es|[N]o) '
+                               ).lower()
+        except EOFError:
+            exit(0)
+        if len(user_input) == 0:
+            prompt_user(secret_num+1)
+        if user_input[0] == 'y':
+            os.system('mpd.sh ; ncmpcpp')
+        elif user_input[0] == 'n':
+            exit(0)
+        else:
+            prompt_user(secret_num+1)
+
+
+if os.system('pidof mpd >/dev/null') == 0:
+    os.system('ncmpcpp')
+
+else:
+    prompt_user()
