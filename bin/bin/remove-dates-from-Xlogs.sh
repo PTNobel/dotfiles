1e4ca1dcb95d4a0425dddf8087b43f485adb0bcf
pnob <pnob99@gmail.com>
2015-03-28 14:51:21 -0700

    Improves remove-dates-from-Xlogs.sh

--- bin/bin/remove-dates-from-Xlogs.sh
+++ bin/bin/remove-dates-from-Xlogs.sh
@@ -12,17 +12,17 @@ export WINDOW_LOG_FILE=$(mktemp)
 
 echo xterm -e zsh -c "echo \$WINDOWID >| $WINDOW_LOG_FILE ; vim $1" &
 xterm -e zsh -c "echo \$WINDOWID >| $WINDOW_LOG_FILE ; vim $1" &
-echo $WINDOW_LOG_FILE; cat $WINDOW_LOG_FILE
+echo "$WINDOW_LOG_FILE"; cat "$WINDOW_LOG_FILE"
 sleep 5
-export WINDOW_ID_OF_VIM=$(cat $WINDOW_LOG_FILE)
-echo $WINDOW_LOG_FILE; cat $WINDOW_LOG_FILE
-echo $WINDOW_ID_OF_VIM
-while sleep .3; cat "$1" | grep "^\[" >|/dev/null ; do
+export WINDOW_ID_OF_VIM=$(cat "$WINDOW_LOG_FILE")
+echo "$WINDOW_LOG_FILE"; cat "$WINDOW_LOG_FILE"
+echo "$WINDOW_ID_OF_VIM"
+while sleep .3; grep "^\[" "$1" >|/dev/null ; do
     echo starting a round
-    xdotool key --window $WINDOW_ID_OF_VIM slash asciicircum backslash bracketleft Return
-    xdotool key --window $WINDOW_ID_OF_VIM 5 d w
-    xdotool key --window $WINDOW_ID_OF_VIM colon w Return
+    xdotool key --window "$WINDOW_ID_OF_VIM" slash asciicircum backslash bracketleft Return
+    xdotool key --window "$WINDOW_ID_OF_VIM" 5 d w
+    xdotool key --window "$WINDOW_ID_OF_VIM" colon w Return
     exit 1
 done
-rm $WINDOW_LOG_FILE
+rm "$WINDOW_LOG_FILE"
 notify-send Done\ with\ this\ file
