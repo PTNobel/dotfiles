04187b945280140d434e881da279317b2e0c60b2
pnob <pnob99@gmail.com>
2015-03-28 15:31:04 -0700

    Improves reload-conky.sh

--- bin/bin/reload-conky.sh
+++ bin/bin/reload-conky.sh
@@ -3,7 +3,7 @@
 killall -q conky
 
 if xrandr | grep '*' | grep '^   1366x768' &>/dev/null
-then setsid conky -c $HOME/.conkyrc-2 &>/dev/null &
+then setsid conky -c "$HOME"/.conkyrc-2 &>/dev/null &
 fi
 
 if xrandr | grep '*' | grep '^   1920x1080' &>/dev/null
