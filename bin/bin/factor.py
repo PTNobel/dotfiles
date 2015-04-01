c95dc13deb40f72b7dc85bf26d78ecb658ee014d
pnob <pnob99@gmail.com>
2015-03-31 23:42:19 -0700

    Makes factor.py closer to factor

--- bin/bin/factor.py
+++ bin/bin/factor.py
@@ -10,17 +10,32 @@ if len(sys.argv) != 2:
     exit(1)
 i = 2
 factors = list()
-orig_num_to_factor = int(sys.argv[1])
+try:
+    orig_num_to_factor = int(sys.argv[1])
+    if float(sys.argv[1]) != float(orig_num_to_factor):
+        print("factor: ‘" + sys.argv[1] + "’ is not a valid positive integer")
+        exit(1)
+    elif orig_num_to_factor < 0:
+        print("factor: ‘" + sys.argv[1] + "’ is not a valid positive integer")
+        exit(1)
+except ValueError:
+    print("factor: ‘" + sys.argv[1] + "’ is not a valid positive integer")
+    exit(1)
 num_to_factor = orig_num_to_factor
+
+
 while i * i < orig_num_to_factor:
     while int(num_to_factor % i) == 0:
         num_to_factor = num_to_factor / i
         factors.append(i)
     i = i + 1
+
+
 if num_to_factor != 1:
     factors.append(int(num_to_factor))
 
 string_to_print = str(orig_num_to_factor) + ": "
+
 for i in factors:
     string_to_print += str(i) + ' '
 
