7f142b065b6b07c27a20f61b0c5801d92622e3c8
pnob <pnob99@gmail.com>
2015-04-08 12:14:08 -0700

    Cleans up CivV.sh

--- bin/bin/CivV.sh
+++ bin/bin/CivV.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
-#cp $HOME/Code/core/monitors.xml.single.screen $HOME/.config/monitors.xml
-#setsid gnome-shell --replace 2>/dev/null &
-#echo "gnome-shell started"
-25xi-layout.sh
-#env DRI_PRIME=1 steam steam://rungameid/8930 &
-steam steam://rungameid/8930 &
-#env DRI_PRIME=1 glxinfo|grep OpenGL\ renderer
-echo "steam started"
-until [ "$(pidof steam | wc -w)" -ge 3 ]
-do sleep 5
-done
-until [ "$(pidof steam | wc -w)" -le 2 ]
-do sleep 30
-done
-#cp $HOME/Code/core/monitors.xml.dual.screen $HOME/.config/monitors.xml
-#setsid gnome-shell --replace 2>/dev/null &
-i3-msg restart
-#sleep 3
-#reload-conky.sh
-exit
+
+
+CivV() {
+    steam steam://rungameid/8930 &
+    echo "steam started"
+    until [ "$(pidof steam | wc -w)" -ge 3 ] ; do
+        sleep 5
+    done
+    until [ "$(pidof steam | wc -w)" -le 2 ] ; do
+        sleep 30
+    done
+}
+
+CivV &
