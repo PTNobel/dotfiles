9ee295a805b22816b3ba1e0586ad5f681bc76ebd
Parth Nobel <pnob99@gmail.com>
2015-04-23 17:16:47 -0700

    Cleans up backup.sh and adds a functional lock.

--- bin/bin/backup.sh
+++ bin/bin/backup.sh
@@ -13,19 +13,19 @@ DUPLICITY_DESTINATION=file://${PRIMARY_DIRECTORY}/Duplicity
 if [ -d "$PRIMARY_DIRECTORY/" ]; then
   if [ -f $DESTINATION/"$USER" ]; then
 
-# No files/directories should be hardcoded
-  echo starting backup.
-  chmod -R +w $DESTINATION
-  echo pwd="$PWD"
-  ls
-  mv $DESTINATION $PRIMARY_DIRECTORY/"$USER"
-  sh -c "rsync -apv --delete --exclude=.cache $HOME $PRIMARY_DIRECTORY ; true"
-  mv $PRIMARY_DIRECTORY/"$USER" $DESTINATION
-  echo "$USER" > $DESTINATION/"$USER"
-  chmod -R a-w $DESTINATION
+    # No files/directories should be hardcoded
+    echo starting backup.
+    mv $DESTINATION $PRIMARY_DIRECTORY/"$USER"
+    chmod -R +w $DESTINATION
+    echo pwd="$PWD"
+    ls
+    sh -c "rsync -apv --delete --exclude=.cache $HOME $PRIMARY_DIRECTORY ; true"
+    echo "$USER" > $DESTINATION/"$USER"
+    chmod -R a-w $DESTINATION
   else echo $DESTINATION not authorized for write by current user ; exit 2
   fi
   echo starting duplicity #; notify-send duplicity started
   duplicity incremental --allow-source-mismatch --no-encryption "$HOME" $DUPLICITY_DESTINATION
+  mv $PRIMARY_DIRECTORY/"$USER" $DESTINATION
 else echo $DESTINATION is not found ; exit 1
 fi
