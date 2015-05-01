73dd9e833019594f6ee7797c128fb280784be5da
Parth Nobel <pnob99@gmail.com>
2015-04-30 21:11:43 -0700

    Removes main() in fix_pulse.sh

--- bin/bin/fix_pulse.sh
+++ bin/bin/fix_pulse.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-main() {
-  ORIG_VOLUME=$(pacmd list-sinks|grep -A 15 "* index: "| \
-                awk '/volume: front/{ print $5 }' | sed 's/%//g')
-  for i in $(pactl list | grep Sink | \
-             awk '/Sink/{print $2}' | grep \# | sed s/\#//); do
-      pactl set-sink-volume "$i" "${ORIG_VOLUME}%"
-  done
-}
-
-main "$@"
+ORIG_VOLUME=$(pacmd list-sinks|grep -A 15 "* index: "| \
+              awk '/volume: front/{ print $5 }' | sed 's/%//g')
+for i in $(pactl list | grep Sink | \
+           awk '/Sink/{print $2}' | grep \# | sed s/\#//); do
+  pactl set-sink-volume "$i" "${ORIG_VOLUME}%"
+done
