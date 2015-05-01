5d295fb8c851662193dc0b05de138a8cdcd6a806
Parth Nobel <pnob99@gmail.com>
2015-04-30 21:10:55 -0700

    Removes unused function.

--- bin/bin/fix_pulse.sh
+++ bin/bin/fix_pulse.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-get_pa_sink() {
-    pactl list | grep Sink | awk '/Sink/{print $2}' | grep \# | sed s/\#//
-}
-
 main() {
   ORIG_VOLUME=$(pacmd list-sinks|grep -A 15 "* index: "| \
                 awk '/volume: front/{ print $5 }' | sed 's/%//g')
