cdb083b5cd21f8b8c2a92e3a50d49efe68404a66
Parth Nobel <pnob99@gmail.com>
2015-05-02 11:00:39 -0700

    Fixes spacing in install.sh

--- install.sh
+++ install.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
 
 stow_wrapper(){
-    for i in $(cat "$MANIFEST_FILE"); do
-        if [ -d "$i" ] || true ; then
-            #echo $i $1
-            echo stow "$1" "$i"
-            stow "$1" "$i"
-        else
-            echo "$i" is not valid content for "$MANIFEST_FILE"
-        fi
-    done
+  for i in $(cat "$MANIFEST_FILE"); do
+    if [ -d "$i" ] || true ; then
+      #echo $i $1
+      echo stow "$1" "$i"
+      stow "$1" "$i"
+    else
+      echo "$i" is not valid content for "$MANIFEST_FILE"
+    fi
+  done
 }
 
 cd "$(dirname "$0")"
 
-export MANIFEST_FILE="$(pwd -P)/manifest.repos"
+MANIFEST_FILE="$(pwd -P)/manifest.repos"
+export MANIFEST_FILE
 if [ "$1" == "-D" ] ; then
-    stow_wrapper -D
-    exit 1
+  stow_wrapper -D
+  exit 1
 elif [ -z "$1" ] ; then
-    stow_wrapper -R
+  stow_wrapper -R
 else
-    echo What was that option you passed me?
-    exit 3
+  echo What was that option you passed me?
+  exit 3
 fi
 
-export MANIFEST_FILE="$(pwd -P)/manifest_disabled.repos"
+MANIFEST_FILE="$(pwd -P)/manifest_disabled.repos"
+export MANIFEST_FILE
 stow_wrapper -D
 
 for i in $(cat "$(pwd -P)"/manifest_build.repos); do
-    cd "$i"
-    make || exit 1
-    make test || exit 1
-    sudo make install || exit 1
-    make clean || exit 1
-    cd "$(dirname "$0")"
+  cd "$i"
+  make || exit 1
+  make test || exit 1
+  sudo make install || exit 1
+  make clean || exit 1
+  cd "$(dirname "$0")"
 done
