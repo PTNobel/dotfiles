f6c79fae71e12e72b102ad0524e71518caa492dc
Parth Nobel <pnob99@gmail.com>
2015-05-03 22:05:12 -0700

    Fixes spacing on latex_watch.sh

--- bin/bin/latex_watch.sh
+++ bin/bin/latex_watch.sh
@@ -3,18 +3,23 @@
 # Watches a latex file for changes and rebuilds it whenever it changes.
 # Exits whenever there is no longer a .swp file.
 
+latex="pdflatex -interaction=batchmode"
+export latex
+
 check_for_changes() {
-    while diff "$1" /tmp/"$(basename "$1")"; do
-        sleep 10
-        if ! [[ -f "$(dirname "$1")"/."$(basename "$1")".swp ]]; then
-            exit 0
-        fi
-    done
-    yes '' | pdflatex "$1"
-    cp "$1" /tmp/"$(basename "$1")"
-    check_for_changes "$1" || exit 1
+  while diff "$1" /tmp/"$(basename "$1")"; do
+    sleep 10
+    if ! [[ -f "$(dirname "$1")"/."$(basename "$1")".swp ]]; then
+      exit 0
+
+    fi
+  done
+
+  $latex "$1"
+  cp "$1" /tmp/"$(basename "$1")"
+  check_for_changes "$1" || exit 1
 }
 
 cp "$1" /tmp/"$(basename "$1")"
-yes '' | pdflatex "$1"
+$latex "$1"
 check_for_changes "$1" || exit 0 && exit 1
