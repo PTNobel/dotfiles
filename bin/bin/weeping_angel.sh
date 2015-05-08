79f2077d90ccf3599c4c6643b487f0f89437f49c
Parth Nobel <pnob99@gmail.com>
2015-05-07 21:43:00 -0700

    Fixes some spacing issues.

--- bin/bin/weeping_angel.sh
+++ bin/bin/weeping_angel.sh
@@ -3,25 +3,16 @@
 # swithces through Weeping Angel photos.
 
 angels() {
-feh --bg-scale ~/Pictures/Weeping\ Angel/1.JPG
-
-sleep 5
-
-feh --bg-scale ~/Pictures/Weeping\ Angel/2.JPG
-
-sleep 5
-
-feh --bg-scale ~/Pictures/Weeping\ Angel/3.JPG
-
-sleep 5
-
-feh --bg-scale ~/Pictures/Weeping\ Angel/4.JPG
-
-sleep 5
-
-feh --bg-scale ~/Pictures/Weeping\ Angel/5.JPG
-
-sleep 5
+  feh --bg-scale ~/Pictures/Weeping\ Angel/1.JPG
+  sleep 5
+  feh --bg-scale ~/Pictures/Weeping\ Angel/2.JPG
+  sleep 5
+  feh --bg-scale ~/Pictures/Weeping\ Angel/3.JPG
+  sleep 5
+  feh --bg-scale ~/Pictures/Weeping\ Angel/4.JPG
+  sleep 5
+  feh --bg-scale ~/Pictures/Weeping\ Angel/5.JPG
+  sleep 5
 }
 
 while true ; do angels ; done
