ad418928d918535dfd7f5969c4a601051475ee76
Parth Nobel <pnob99@gmail.com>
2015-05-08 17:23:09 -0700

    Rewrite of backup.sh

--- bin/bin/backup.sh
+++ bin/bin/backup.sh
@@ -7,28 +7,31 @@ set -u
 
 #DEFINTIONS!
 PRIMARY_DIRECTORY=/media/Toshiba_Backups
-DESTINATION=${PRIMARY_DIRECTORY}/Backups
+PRIMARY_DIRECTORY_ntfs=/media/Toshiba_Backups_ntfs
+DESTINATION=$PRIMARY_DIRECTORY_ntfs/Backups
 DUPLICITY_DESTINATION=file://${PRIMARY_DIRECTORY}/Duplicity
+export PRIMARY_DIRECTORY PRIMARY_DIRECTORY_ntfs DESTINATION DUPLICITY_DESTINATION
+
+backup_home() {
+  # No files/directories should be hardcoded
+  echo starting backup.
+  sh -c "rsync -apv --delete --exclude=.cache $HOME $PRIMARY_DIRECTORY_ntfs; true"
+  echo "$USER" > $PRIMARY_DIRECTORY_ntfs/"$USER"/"$USER"
+}
 
 if [ -d "$PRIMARY_DIRECTORY/" ]; then
   if [ -f $DESTINATION/"$USER" ]; then
+    mv $DESTINATION $PRIMARY_DIRECTORY_ntfs/"$USER"
+    backup_home &
 
-    # No files/directories should be hardcoded
-    echo starting backup.
-    mv $DESTINATION $PRIMARY_DIRECTORY/"$USER"
-    chmod -R +w $PRIMARY_DIRECTORY/"$USER"
-    echo pwd="$PWD"
-    ls
-    sh -c "rsync -apv --delete --exclude=.cache $HOME $PRIMARY_DIRECTORY ; true"
-    echo "$USER" > $PRIMARY_DIRECTORY/"$USER"/"$USER"
-    chmod -R a-w $PRIMARY_DIRECTORY/"$USER"
   else
     echo $DESTINATION not authorized for write by current user ; exit 2
   fi
 
   echo starting duplicity #; notify-send duplicity started
-  duplicity incremental --allow-source-mismatch --no-encryption "$HOME" $DUPLICITY_DESTINATION
-  mv $PRIMARY_DIRECTORY/"$USER" $DESTINATION
+  duplicity incremental --allow-source-mismatch --no-encryption "$HOME" $DUPLICITY_DESTINATION &
+  wait
+  mv $PRIMARY_DIRECTORY_ntfs/"$USER" $DESTINATION
 else
   echo $DESTINATION is not found ; exit 1
 fi
