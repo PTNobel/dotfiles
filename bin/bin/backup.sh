a381c1220966da8e6c16125d992ea3518a0b570d
Parth Nobel <pnob99@gmail.com>
2015-05-09 11:51:18 -0700

    Finishes backup.sh

--- bin/bin/backup.sh
+++ bin/bin/backup.sh
@@ -29,9 +29,14 @@ if [ -d "$PRIMARY_DIRECTORY/" ]; then
   fi
 
   echo starting duplicity #; notify-send duplicity started
-  duplicity incremental --allow-source-mismatch --no-encryption "$HOME" $DUPLICITY_DESTINATION &
+  bash -c 'duplicity incremental --allow-source-mismatch --no-encryption "$HOME" $DUPLICITY_DESTINATION ; true' &
   wait
   mv $PRIMARY_DIRECTORY_ntfs/"$USER" $DESTINATION
+  # Cleaning up.
+  cd /media
+  if udevil umount $PRIMARY_DIRECTORY && udevil umount $PRIMARY_DIRECTORY_ntfs; then
+    udisksctl power-off --no-user-interaction -b '/dev/disk/by-id/usb-TOSHIBA_External_USB_3.0_20140730015698-0:0'
+  fi
 else
   echo $DESTINATION is not found ; exit 1
 fi
