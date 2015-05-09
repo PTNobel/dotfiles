4e7be4bbcf835b913c347b7a826185886e45aa52
Parth Nobel <pnob99@gmail.com>
2015-05-09 12:16:10 -0700

    Merges fix_pulse.sh and pulsectl

--- bin/bin/fix_pulse.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ORIG_VOLUME=$(pacmd list-sinks|grep -A 15 "* index: "| \
-              awk '/volume: front/{ print $5 }' | sed 's/%//g')
-for i in $(pactl list | grep Sink | \
-           awk '/Sink/{print $2}' | grep \# | sed s/\#//); do
-  pactl set-sink-volume "$i" "${ORIG_VOLUME}%"
-done
