61fa055686d0cfcf8315f4d8ddc4f6d5e6281910
Parth Nobel <pnob99@gmail.com>
2015-05-13 20:48:57 -0700

    Rewrites backup.sh

--- bin/bin/backup.sh
+++ bin/bin/backup.sh
@@ -5,6 +5,14 @@
 set -e
 set -u
 
+keep_computer_awake() {
+  systemd-inhibit bash -c "while [[ -f /tmp/backup.lock ]] ; do sleep 60 ; done"
+}
+
+exit() {
+  rm /tmp/backup.lock
+  builtin exit "$@"
+}
 #DEFINTIONS!
 PRIMARY_DIRECTORY=/media/Toshiba_Backups
 PRIMARY_DIRECTORY_ntfs=/media/Toshiba_Backups_ntfs
@@ -12,13 +20,16 @@ DESTINATION=$PRIMARY_DIRECTORY_ntfs/Backups
 DUPLICITY_DESTINATION=file://${PRIMARY_DIRECTORY}/Duplicity
 export PRIMARY_DIRECTORY PRIMARY_DIRECTORY_ntfs DESTINATION DUPLICITY_DESTINATION
 
+touch /tmp/backup.lock
+keep_computer_awake &
+
 backup_home() {
   # No files/directories should be hardcoded
   echo starting backup.
   sh -c "rsync -apv --delete --exclude=.cache $HOME $PRIMARY_DIRECTORY_ntfs; true"
   echo "$USER" > $PRIMARY_DIRECTORY_ntfs/"$USER"/"$USER"
 }
-
+main() {
 if [ -d "$PRIMARY_DIRECTORY/" ]; then
   if [ -f $DESTINATION/"$USER" ]; then
     mv $DESTINATION $PRIMARY_DIRECTORY_ntfs/"$USER"
@@ -41,3 +52,8 @@ if [ -d "$PRIMARY_DIRECTORY/" ]; then
 else
   echo $DESTINATION is not found ; exit 1
 fi
+}
+
+main
+
+rm /tmp/backup.lock
