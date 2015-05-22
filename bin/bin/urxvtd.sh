d443fc04a4387d87db1517c96dd96ec7591f9669
Parth Nobel <pnob99@gmail.com>
2015-05-22 10:34:23 -0700

    Fixes ts in urxvtd.sh

--- bin/bin/urxvtd.sh
+++ bin/bin/urxvtd.sh
@@ -3,15 +3,15 @@
 # keeps urxvtd running.
 
 for i in $(pidof -x urxvtd.sh -o %PPID) ; do
-    if strings /proc/"$i"/environ | grep DISPLAY | grep "${DISPLAY}" >/dev/null ; then
-        kill "$i"
-    fi
+  if strings /proc/"$i"/environ | grep DISPLAY | grep "${DISPLAY}" >/dev/null ; then
+    kill "$i"
+  fi
 done
 
 while true ; do
-    if [ "$(pidof urxvtd | wc -w)" -lt 1 ]; then
-        urxvtd
-else
+  if [ "$(pidof urxvtd | wc -w)" -lt 1 ]; then
+    urxvtd
+  else
     sleep 10
-fi
+  fi
 done
