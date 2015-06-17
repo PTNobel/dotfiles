741d6b807f9fc9af8e927ca4b3bdb5363d497c53
Parth Nobel <pnob99@gmail.com>
2015-06-17 15:44:29 -0700

    Rewrites musicctl.py takes care of python scope.

--- bin/bin/musicctl.py
+++ bin/bin/musicctl.py
@@ -14,16 +14,18 @@ def warning(*objs, prefix='WARNING: '):
     print(printed_list, file=sys.stderr)
 
 
-def usage(exit_code, name_of_program='musicctl.py'):
-    verboseprint('usage() starting', exit_code, name_of_program)
-    usage_text = "Usage: " + name_of_program + \
+def usage(exit_code, name):
+    verboseprint('usage() starting', exit_code, name)
+    usage_text = "Usage: " + name + \
         " {[a command]|player|commands|usage|help}"
+
     if exit_code == 0:
         print(usage_text)
     elif exit_code > 0:
         warning(usage_text, prefix='')
-    else:
-        exit_code = abs(exit_code)
+    elif exit_code < 0:
+        verboseprint('Wait a negative exit_code to usage(), what?')
+        usage(exit_code, name)
     exit(exit_code)
 
 
@@ -45,52 +47,54 @@ verboseprint("Defining verboseprint")
 def processargs(input_argv):
     verboseprint('start proccessargs')
 
-    def help(input_dict):
-        usage(0, input_dict["name"])
-        return input_dict
+    def help():
+        usage(0, output['name'])
 
-    def verbose(input_dict):
-        input_dict["verbose"] = True
-        input_dict["test_mode_suffix"] = ''
-        return input_dict
+    def verbose():
+        output["verbose"] = True
+        output["test_mode_suffix"] = ''
 
-    def trial(input_dict):
-        input_dict["test_mode_prefix"] = 'echo '
-        input_dict["test_mode_suffix"] = ''
-        return input_dict
+    def trial():
+        output["test_mode_prefix"] = 'echo '
+        output["test_mode_suffix"] = ''
+
+    def player():
+        output["player"] = input_argv[i].split('=')[1]
 
     long_args_to_disc = {'--help': help, '--verbose': verbose,
-                         '--trial': trial}
-    short_args_to_disc = {'h': help, 'v': verbose,
-                          't': trial}
+                         '--trial': trial, '--player': player}
+    short_args_to_disc = {'h': help, 'v': verbose, 't': trial}
     output = {"verbose": None, "input": None, 'test_mode_prefix': '',
-              'test_mode_suffix': ' >/dev/null', 'argv': input_argv,
-              "name": os.path.basename(input_argv[0]),
+              'test_mode_suffix': ' >/dev/null',
+              "name": os.path.basename(input_argv[0]), "player": None,
               }
     if len(input_argv) == 1:
         warning("Not enough arguments")
-        usage(1, output["name"])
+        usage(1, output['name'])
     else:
+        # range() starts at 1 to prevent the name from being processed.
         for i in range(1, len(input_argv)):
             verboseprint("Index:", i, input_argv, output)
             if len(input_argv[i]) >= 2 and input_argv[i][0:2] == '--':
                 try:
-                    verboseprint(long_args_to_disc[input_argv[i]])
-                    output = long_args_to_disc[input_argv[i]](output)
+                    verboseprint(long_args_to_disc[
+                        input_argv[i].split('=')[0]])
+                    long_args_to_disc[input_argv[i].split('=')[0]]()
                     verboseprint(output)
                 except KeyError:
+                    verboseprint(input_argv[i].split('=')[0])
                     warning("Invalid argument", prefix='')
-                    usage(1, output["name"])
+                    usage(1, output['name'])
 
             elif input_argv[i][0] == '-' and input_argv[i][1] != '-':
                 for j in range(1, len(input_argv[i])):
                     try:
                         verboseprint(short_args_to_disc[input_argv[i][j]])
-                        output = short_args_to_disc[input_argv[i][j]](output)
+                        short_args_to_disc[input_argv[i][j]]()
                         verboseprint(output)
                     except KeyError:
                         warning("Invalid argument", prefix='')
-                        usage(1, output["name"])
+                        usage(1, output['name'])
 
             elif output["input"] is None:
                 output["input"] = input_argv[i]
@@ -103,16 +107,17 @@ def processargs(input_argv):
                     i,
                     input_argv[i],
                     output["input"])
-                usage(1, output["name"])
+                usage(1, output['name'])
 
     verboseprint('returning', output)
     verboseprint('end processargs')
     return output
 
+# global arguments
+arguments = processargs(sys.argv)
 
-processed_args = processargs(sys.argv)
 
-if processed_args["verbose"]:
+if arguments["verbose"]:
     def verboseprint(*args):
         # Print each argument separately so caller doesn't need to stuff
         # everything to be printed into a string.
@@ -131,15 +136,16 @@ def get_keys(command_dict):
         print("For player " + i + " the following commands are available:")
         for j in sorted(command_dict[i].keys()):
             print("   " + j)
-    usage(-256, '')
+
+    exit(0)
 
 
 class mpd:
 
-    def __init__(self, processed_args):
+    def __init__(self):
         verboseprint('mpd is being inited')
-        self.system_prefix = processed_args['test_mode_prefix']
-        self.system_suffix = processed_args['test_mode_suffix']
+        self.system_prefix = arguments['test_mode_prefix']
+        self.system_suffix = arguments['test_mode_suffix']
         self.commands = {'mpd': {'play': self.pause, 'pause': self.pause,
                                  'back': self.back, 'next': self.next,
                                  'quit': self.stop, 'stop': self.stop}}
@@ -163,10 +169,10 @@ class mpd:
 
 class pianobar:
 
-    def __init__(self, processed_args):
+    def __init__(self):
         verboseprint('pianobar is being inited')
-        self.system_prefix = processed_args['test_mode_prefix']
-        self.system_suffix = processed_args['test_mode_suffix']
+        self.system_prefix = arguments['test_mode_prefix']
+        self.system_suffix = arguments['test_mode_suffix']
         self.commands = {'pianobar': {'play': self.pause, 'pause': self.pause,
                                       'back': self.like, 'next': self.next,
                                       'quit': self.stop, 'stop': self.stop,
@@ -198,10 +204,10 @@ class pianobar:
 
 class playerctl:
 
-    def __init__(self, processed_args):
+    def __init__(self):
         verboseprint('playerctl is being inited')
-        self.system_prefix = processed_args['test_mode_prefix']
-        self.system_suffix = processed_args['test_mode_suffix']
+        self.system_prefix = arguments['test_mode_prefix']
+        self.system_suffix = arguments['test_mode_suffix']
         self.commands = {'playerctl': {'play': self.pause, 'pause': self.pause,
                                        'back': self.back, 'next': self.next,
                                        'quit': self.stop, 'stop': self.stop}}
@@ -225,27 +231,30 @@ class playerctl:
         os.system(self.system_prefix + 'playerctl stop' + self.system_suffix)
 
 
-def which_player():
+def which_player(arguments):
     output = str()
-    processes = [
-        open(os.path.join('/proc', pid, 'comm'), 'r').read().strip('\n')
-        for pid in os.listdir('/proc') if pid.isdigit()]
-
-    verboseprint(processes)
-
-    if 'mpd' in processes:
-        # pianobar get priority over mpd, unless mpd is playing.
-        if 'pianobar' in processes:
-            if os.system("mpc status | grep playing &>/dev/null") == 0:
-                output = "mpd"
-            else:
-                output = "pianobar"
-        else:
-            output = "mpd"
-    elif 'pianobar' in processes:
-        output = "pianobar"
+    if arguments['player'] is not None:
+        output = arguments['player']
     else:
-        output = 'playerctl'
+        processes = [
+            open(os.path.join('/proc', pid, 'comm'), 'r').read().strip('\n')
+            for pid in os.listdir('/proc') if pid.isdigit()]
+
+        verboseprint(processes)
+
+        if 'mpd' in processes:
+            # pianobar get priority over mpd, unless mpd is playing.
+            if 'pianobar' in processes:
+                if os.system("mpc status | grep playing &>/dev/null") == 0:
+                    output = "mpd"
+                else:
+                    output = "pianobar"
+            else:
+                output = "mpd"
+        elif 'pianobar' in processes:
+            output = "pianobar"
+        else:
+            output = 'playerctl'
     return output
 
 
@@ -254,19 +263,19 @@ def main(arguments):
     verboseprint(arguments)
     # Handle help and usage correctly:
     if arguments["input"] == "usage" or arguments["input"] == "help":
-        usage(0, arguments["name"])
+        usage(0, arguments['name'])
 
     # Figure out what player is running.
-    player = which_player()
+    player = which_player(arguments)
     if arguments["input"] == "player":
         print(player)
-        usage(-256, arguments['name'])
+        exit(0)
 
     # Create a two dimensional dictionary. first key specifies player and the
     # second one is the specific command. It'll be the function to call. It's
     # possible to define commands that are specific to a player.
     commands = dict()
-    for i in [pianobar(arguments), mpd(arguments), playerctl(arguments)]:
+    for i in [pianobar(), mpd(), playerctl()]:
         commands.update(i.commands)
 
     verboseprint(commands)
@@ -282,8 +291,8 @@ def main(arguments):
         commands[player][arguments["input"]]()
     except KeyError:
         warning("Invalid input.")
-        usage(1, arguments["name"])
+        usage(1, arguments['name'])
 
 
 if __name__ == "__main__":
-    main(processed_args)
+    main(arguments)
