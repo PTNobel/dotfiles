abd22a00bee2fca33706c4eb8588fa08090f0757
Parth Nobel <pnob99@gmail.com>
2015-06-17 15:44:01 -0700

    Switches back to using steam to launch Civ.

--- bin/bin/CivV.sh
+++ bin/bin/CivV.sh
@@ -3,9 +3,9 @@
 # Launches CivV.
 
 CivV() {
-  #steam steam://rungameid/8930 &
-  cd /home/parth/.local/share/Steam/SteamApps/common/Sid\ Meier\'s\ Civilization\ V
-  /home/parth/.local/share/Steam/SteamApps/common/Sid\ Meier\'s\ Civilization\ V/Civ5XP
+  steam steam://rungameid/8930 &
+  #cd /home/parth/.local/share/Steam/SteamApps/common/Sid\ Meier\'s\ Civilization\ V
+  #/home/parth/.local/share/Steam/SteamApps/common/Sid\ Meier\'s\ Civilization\ V/Civ5XP
   echo "steam started"
   until [ "$(pidof steam | wc -w)" -ge 3 ] ; do
     sleep 5
