6dab3e161fb7f7d99e2230b7063384f190a302c2
Parth Nobel <pnob99@gmail.com>
2015-06-17 15:47:11 -0700

    Moves musicctl.sh to ts=2

--- bin/bin/musicctl.sh
+++ bin/bin/musicctl.sh
@@ -8,93 +8,93 @@
 
 # Pianobar or mpd?
 get_player() {
-    if pidof pianobar >/dev/null; then
-       if pidof mpd >/dev/null && mpc | grep playing >/dev/null; then
-         player=mpd
-       else
-         player=pianobar
-       fi
-    elif pidof mpd >/dev/null; then
-        player=mpd
+  if pidof pianobar >/dev/null; then
+    if pidof mpd >/dev/null && mpc | grep playing >/dev/null; then
+      player=mpd
     else
-        exit 1
+      player=pianobar
     fi
-    export $player
+  elif pidof mpd >/dev/null; then
+    player=mpd
+  else
+    exit 1
+  fi
+  export $player
 }
 
 self_pause() {
-   get_player
-   if [ "$player" == "pianobar" ]; then
-      pianoctl p
-   elif [ "$player" == "mpd" ]; then
-      mpc toggle >/dev/null
-   else
-      exit 1
-   fi
+  get_player
+  if [ "$player" == "pianobar" ]; then
+    pianoctl p
+  elif [ "$player" == "mpd" ]; then
+    mpc toggle >/dev/null
+  else
+    exit 1
+  fi
 }
 
 self_play() {
-    self_pause
+  self_pause
 }
 
 self_back() {
-   get_player
-   if [ "$player" == "pianobar" ]; then
-      pianoctl +
-   elif [ "$player" == "mpd" ]; then
-      mpc prev >/dev/null
-   else
-      exit 1
-   fi
+  get_player
+  if [ "$player" == "pianobar" ]; then
+    pianoctl +
+  elif [ "$player" == "mpd" ]; then
+    mpc prev >/dev/null
+  else
+    exit 1
+  fi
 }
 
 self_next() {
-   get_player
-   if [ "$player" == "pianobar" ]; then
-      pianoctl -
-   elif [ "$player" == "mpd" ]; then
-      mpc next >/dev/null
-   else
-      exit 1
-   fi
+  get_player
+  if [ "$player" == "pianobar" ]; then
+    pianoctl -
+  elif [ "$player" == "mpd" ]; then
+    mpc next >/dev/null
+  else
+    exit 1
+  fi
 }
 
 self_stop() {
-   get_player
-   if [ "$player" == "pianobar" ]; then
-      pianoctl q
-   elif [ "$player" == "mpd" ]; then
-      mpc stop >/dev/null
-   else
-      exit 1
-   fi
+  get_player
+  if [ "$player" == "pianobar" ]; then
+    pianoctl q
+  elif [ "$player" == "mpd" ]; then
+    mpc stop >/dev/null
+  else
+    exit 1
+  fi
 }
 
 self_quit() {
-   self_stop
+  self_stop
 }
 
 self_usage() {
-   printf "Usage:
-   %s pause
-   %s play
-   %s back
-   %s next
-   %s stop
-   %s quit\n" "$0" "$0" "$0" "$0" "$0" "$0"
+  printf "Usage:
+  %s pause
+  %s play
+  %s back
+  %s next
+  %s stop
+  %s quit\n" "$0" "$0" "$0" "$0" "$0" "$0"
 }
 
 self_help() {
-   self_usage
+  self_usage
 }
 
 self_-h() {
-   self_usage
+  self_usage
 }
 
 self_() {
-   self_usage
-   exit 1
+  self_usage
+  exit 1
 }
 
 self_"$1" || exit 1
