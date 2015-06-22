9b358aeb6fedaca4bf1d32460df02edb46bffa1e
Parth Nobel <pnob99@gmail.com>
2015-06-21 19:52:47 -0700

    Adds the ~/.backup.block support to backup.sh

--- bin/bin/backup.sh
+++ bin/bin/backup.sh
@@ -20,6 +20,7 @@ DESTINATION=$PRIMARY_DIRECTORY_ntfs/Backups
 DUPLICITY_DESTINATION=file://${PRIMARY_DIRECTORY}/Duplicity
 export PRIMARY_DIRECTORY PRIMARY_DIRECTORY_ntfs DESTINATION DUPLICITY_DESTINATION
 
+if [[ -f ~/.backup.block ]] ; then cat ~/.backup.block ; builtin exit ; fi
 if [[ -f /tmp/backup.lock ]] ; then echo lock file is present ; builtin exit ;
 else touch /tmp/backup.lock; fi
 echo "Made lock file"
@@ -32,19 +33,22 @@ backup_home() {
   sh -c "rsync -apv --delete --exclude=.cache $HOME $PRIMARY_DIRECTORY_ntfs; true"
   echo "$USER" > $PRIMARY_DIRECTORY_ntfs/"$USER"/"$USER"
   mv $PRIMARY_DIRECTORY_ntfs/"$USER" $DESTINATION
+  echo rsync >> /tmp/backup.lock
 }
 
 main() {
 if [ -d "$PRIMARY_DIRECTORY/" ]; then
   if [ -f $DESTINATION/"$USER" ]; then
     backup_home &
+    PID_BACKUP_HOME=$!
   else
     echo $DESTINATION not authorized for write by current user ; exit 2
   fi
 
   echo starting duplicity #; notify-send duplicity started
-  bash -c 'duplicity incremental --allow-source-mismatch --no-encryption "$HOME" $DUPLICITY_DESTINATION ; true' &
-  wait
+  bash -c 'duplicity incremental --allow-source-mismatch --no-encryption "$HOME" "$DUPLICITY_DESTINATION" ; echo duplicity >> /tmp/backup.lock; true' &
+  PID_DUPLICITY=$!
+  wait $PID_DUPLICITY $PID_BACKUP_HOME
   # Cleaning up.
   cd /media
   if udevil umount $PRIMARY_DIRECTORY && udevil umount $PRIMARY_DIRECTORY_ntfs; then
