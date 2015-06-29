5706dcddd2288be8d8114ed638ea2afbc73bd163
Parth Nobel <pnob99@gmail.com>
2015-06-28 23:24:13 -0700

    Sleep 3 seconds before cleaning up in update.sh

--- bin/bin/update.sh
+++ bin/bin/update.sh
@@ -102,5 +102,6 @@ tail -n"$(wc -l < "$OUTPUT_FILE")"  -f "$OUTPUT_FILE" | lolcat &
 export TAILPID=$!
 
 wait $PID1 $PID2 $PID3 $PID4 $PID5 $PID6 $PID7 $PID8
+sleep 3
 echo wait finished.
 exit_routine
