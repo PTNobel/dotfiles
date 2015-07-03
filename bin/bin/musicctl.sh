a16964e4e4586a6b631d6b267592caf4165b0df4
Parth Nobel <pnob99@gmail.com>
2015-07-03 12:57:54 -0700

    Complete rewrite of musicctl.sh to use case statements

--- bin/bin/musicctl.sh
+++ bin/bin/musicctl.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 #
-# This code is poorly written just an FYI.
-# It should work. However musicctl.py is much better.
-# This code is probably insecure if someone really tries to exploit it.
-# So don't use this in any situation where the user doesn't already have
-# shell access.
+#
 
-# Pianobar or mpd?
 get_player() {
   if pidof pianobar >/dev/null; then
     if pidof mpd >/dev/null && mpc | grep playing >/dev/null; then
@@ -22,96 +17,70 @@ get_player() {
   export $player
 }
 
-self_pause() {
-  get_player
-  if [ "$player" == "pianobar" ]; then
-    pianoctl p
-  elif [ "$player" == "mpd" ]; then
-    mpc toggle >/dev/null
-  else
-    exit 1
-  fi
-}
+get_player
 
-self_play() {
-  self_pause
-}
-
-self_back() {
-  get_player
-  if [ "$player" == "pianobar" ]; then
-    pianoctl +
-  elif [ "$player" == "mpd" ]; then
-    mpc prev >/dev/null
-  else
-    exit 1
-  fi
-}
-
-self_next() {
-  get_player
-  if [ "$player" == "pianobar" ]; then
-    pianoctl -
-  elif [ "$player" == "mpd" ]; then
-    mpc next >/dev/null
-  else
-    exit 1
-  fi
-}
-
-self_stop() {
-  get_player
-  if [ "$player" == "pianobar" ]; then
-    pianoctl q
-  elif [ "$player" == "mpd" ]; then
-    mpc stop >/dev/null
-  else
-    exit 1
-  fi
-}
-
-self_quit() {
-  self_stop
-}
-
-self_is_playing() {
-  get_player
-  if [ "$player" == "pianobar" ]; then
-    LOGFILE_1="$(mktemp)"
-    LOGFILE_2="$(mktemp)"
-    strings  ~/.config/pianobar/out | tail -n1 >> "$LOGFILE_1"
-    sleep 2
-    strings  ~/.config/pianobar/out | tail -n1 >> "$LOGFILE_2"
-    if ! diff "$LOGFILE_1" "$LOGFILE_2" ; then EXIT=0; else EXIT=1;fi #&>/dev/null
-    rm "$LOGFILE_1" "$LOGFILE_2"
-    exit $EXIT
-  elif [ "$player" == "mpd" ]; then
-    mpc status | grep playing
-    exit $?
-  fi
-}
-
-self_usage() {
-  printf "Usage:
-  %s pause
-  %s play
-  %s back
-  %s next
-  %s stop
-  %s quit\n" "$0" "$0" "$0" "$0" "$0" "$0"
-}
-
-self_help() {
-  self_usage
-}
-
-self_-h() {
-  self_usage
-}
-
-self_() {
-  self_usage
-  exit 1
-}
-
-self_"$1" || exit 1
+case "$1" in
+  ""|-h|help|usage)
+    NAME="$(basename "$0")"
+    printf "Usage:
+      %s pause
+      %s play
+      %s back
+      %s next
+      %s stop
+      %s quit
+      %s is_playing\n" "$NAME" "$NAME" "$NAME" "$NAME" "$NAME" "$NAME" "$NAME"
+      ;;
+  *)
+    case "$player" in
+      pianobar)
+        case "$1" in
+          pause|play)
+            pianoctl p
+            ;;
+          back|like)
+            pianoctl +
+            ;;
+          next|dislike)
+            pianoctl -
+            ;;
+          stop|quit)
+            pianoctl q
+            ;;
+          is_playing)
+            LOGFILE_1="$(mktemp)"
+            LOGFILE_2="$(mktemp)"
+            strings  ~/.config/pianobar/out | tail -n1 >> "$LOGFILE_1"
+            sleep 2
+            strings  ~/.config/pianobar/out | tail -n1 >> "$LOGFILE_2"
+            if ! diff "$LOGFILE_1" "$LOGFILE_2" ; then
+              EXIT=0
+            else
+              EXIT=1
+            fi &>/dev/null
+            rm "$LOGFILE_1" "$LOGFILE_2"
+            exit $EXIT
+            ;;
+        esac
+        ;;
+      mpd)
+        case "$1" in
+          pause|play)
+            mpc toggle >/dev/null
+            ;;
+          back)
+            mpc prev >/dev/null
+            ;;
+          next)
+            mpc next >/dev/null
+            ;;
+          stop|quit)
+            mpc stop >/dev/null
+            ;;
+          is_playing)
+            mpc status | grep playing >/dev/null
+            exit $?
+            ;;
+        esac
+    esac
+esac
