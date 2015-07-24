c80b734181db9ebe64a101db39c2092311e487de
Parth Nobel <pnob99@gmail.com>
2015-07-23 22:14:58 -0700

    Improves latex_watch.sh

--- bin/bin/latex_watch.sh
+++ bin/bin/latex_watch.sh
@@ -4,15 +4,15 @@
 # Exits whenever there is no longer a .swp file.
 #LATEX="$LATEX"
 if [ "$2" == "xelatex" ]; then
-  latex="xelatex  -output-directory /tmp -interaction=nonstopmode"
+  latex="xelatex  -output-directory /tmp/${USER}-LaTeX -interaction=nonstopmode"
 else
-  latex="pdflatex -output-directory /tmp -interaction=nonstopmode"
+  latex="pdflatex -output-directory /tmp/${USER}-LaTeX -interaction=nonstopmode"
 fi
 export latex
 
 function check_for_changes {
   $latex "$1"
-  while diff "$1" /tmp/"$(basename "$1")"; do
+  while diff "$1" /tmp/"${USER}"-LaTeX/"$(basename "$1")"; do
     sleep 5
     if ! [[ -f "$(dirname "$1")"/."$(basename "$1")".swp ]]; then
       $latex "$1"
@@ -21,11 +21,12 @@ function check_for_changes {
     fi
   done
   #$latex "$1"
-  cp "$1" /tmp/"$(basename "$1")"
+  cp "$1" /tmp/"${USER}"-LaTeX/"$(basename "$1")"
   check_for_changes "$1" || exit 1
 }
 
-cp "$1" /tmp/"$(basename "$1")"
+mkdir -p /tmp/"${USER}"-LaTeX
+cp "$1" /tmp/"${USER}"-LaTeX/"$(basename "$1")"
 $latex "$1"
-rifle /tmp/"$(basename "$1" .tex)".pdf
+rifle /tmp/"${USER}"-LaTeX/"$(basename "$1" .tex)".pdf
 check_for_changes "$1" || exit 0 && exit 1
