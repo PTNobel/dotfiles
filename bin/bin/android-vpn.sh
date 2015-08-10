a02e82215253b97d25374761d31fac5696180de9
Parth Nobel <pnob99@gmail.com>
2015-08-10 12:30:08 -0700

    Fix android-vpn

--- bin/bin/android-vpn.sh
+++ bin/bin/android-vpn.sh
@@ -6,7 +6,4 @@ adb forward tcp:41927 tcp:41927
 
 cd /usr/local/share/vpn.sh/
 
-if wifi | grep on; then sudo wifi off; else
-  sudo setsid openvpn azilink.ovpn
-fi
-sudo wifi on
+sudo openvpn azilink.ovpn
