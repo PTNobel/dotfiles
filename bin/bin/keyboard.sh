6f5294e0457379555d382781f91fbc7410e32236
Parth Nobel <pnob99@gmail.com>
2016-01-07 21:29:59 -0800

    Adds the caps -> escape into keyboard.sh

--- bin/bin/keyboard.sh
+++ bin/bin/keyboard.sh
@@ -6,7 +6,9 @@ VARIANT=$(setxkbmap -query | awk '/variant/{print $2}')
 
 if [ "$VARIANT" == "intl" ] ; then
   setxkbmap -layout us -variant ''
+  setxkbmap -option caps:escape
 
 else
   setxkbmap -layout us -variant intl #; echo " VARIANT = intl "
+  setxkbmap -option caps:escape
 fi
