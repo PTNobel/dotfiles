85e2d86c97544eb3278fa47e57d7d8b1d8cd46d9
Parth Nobel <pnob99@gmail.com>
2016-01-08 00:21:05 -0800

    Adds music.py

---
+++ bin/bin/music.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+import subprocess
+import process
+import time
+
+
+def prompt_user(secret_num=0):
+    if secret_num >= 5:
+        exit(1)
+    else:
+        try:
+            user_input = input('Do you want to launch mpd, pianobar,' +
+                               ' or neither? ([m]|[p]]|[n]) '
+                               ).lower()
+        except EOFError:
+            exit(0)
+        if len(user_input) == 0:
+            prompt_user(secret_num + 1)
+        elif user_input[0] == 'm':
+            subprocess.call(['mpd.sh'])
+            subprocess.call(['ncmpcpp'])
+        elif user_input[0] == 'p':
+            subprocess.call(['pianoctl'])
+        elif user_input[0] == 'n':
+            exit(0)
+        else:
+            prompt_user(secret_num + 1)
+
+
+def main():
+    process.update_buffers()
+    if process.is_comm_running('pianobar'):
+        subprocess.call(['pianoctl'])
+
+    elif process.is_comm_running('mpd'):
+        subprocess.call(['ncmpcpp'])
+
+    else:
+        prompt_user()
+
+if __name__ == '__main__':
+    while True:
+        main()
+        time.sleep(5)
