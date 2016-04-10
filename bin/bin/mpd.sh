b6977052f718b813959b77b6f0337797809d2dab
Parth Nobel <pnob99@gmail.com>
2016-04-10 13:59:38 -0700

    Makes a change to mpd.sh

--- bin/bin/mpd.sh
+++ bin/bin/mpd.sh
@@ -10,5 +10,5 @@ mpd ~/.config/mpd/mpd.conf || mpd ~/.config/mpd/mpd.conf.offline
 if [ $? -eq 0 ]; then
   mpd #~/.config/mpd/mpd.conf.online
 else
-  mpd ~/.config/mpd/mpd.conf
+  mpd ~/.config/mpd/mpd.conf.offline
 fi
