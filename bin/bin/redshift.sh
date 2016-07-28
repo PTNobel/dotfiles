2e950fbfb311ca71bae8f6efb10a8e03b532c9ef
Parth Nobel <pnob99@gmail.com>
2016-07-27 23:18:18 -0700

    redshift.py now has a third setting: off

--- bin/bin/redshift.sh
+++ bin/bin/redshift.sh
@@ -4,17 +4,19 @@
 
 if pidof redshift; then
     echo redshift is running
-    cd /proc
+    cd /proc || exit 54
     for i in $(pidof redshift) ; do
     echo "$i"
-    cd "$i"
+    cd "$i" || exit 54
     if [ "$(strings ./environ | grep DISPLAY)" = "DISPLAY=${DISPLAY}" ]; then
         OPTIONS=$(cat cmdline)
         kill "$i"
     fi
     done
     if echo "$OPTIONS" | grep redshift.alt.conf >/dev/null ; then
-        redshift &
+        redshift -c "$HOME"/.config/redshift.off.conf &
+    elif echo "$OPTIONS" | grep redshift.off.conf ; then
+      redshift &
     else
         redshift -c "$HOME"/.config/redshift.alt.conf &
     fi
