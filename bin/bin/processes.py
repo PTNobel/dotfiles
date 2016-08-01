8e2cfdb1a49b21eed21225614ce083704c4e4e51
Parth Nobel <pnob99@gmail.com>
2016-08-01 01:10:29 -0700

    Reimplements processes.py based off the new process.py

--- bin/bin/processes.py
+++ bin/bin/processes.py
@@ -1,130 +1,109 @@
 #!/usr/bin/python3
 
+__all__ = [
+    "Processes",
+]
 
 from os import listdir as _listdir
 from os.path import join as _join
+from typing import List, Dict
+from time import ctime as _ctime
 
 
 class Processes:
-    _buffer_map_pids_to_comms = dict()
-    _buffer_map_comms_to_pids = dict()
-    _buffer_running_pids = list()
-    _buffer_list_of_comms = list()
+    _buffer_map_pids_to_comms = {}  # type: Dict[str, str]
+    _buffer_map_pids_to_cmdlines = {}  # type: Dict[str, List[str]]
+    _buffer_map_comms_to_pids = {}  # type: Dict[str, List[str]]
+    _buffer_running_pids = []  # type: List[str]
+    _buffer_list_of_comms = []  # type: List[str]
+    time_of_init = ""
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and \
+            self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __repr__(self):
+        return "<Processes at " + self.time_of_init + ">"
 
     def __init__(self):
-        self._build_buffers(False)
+        self._buffer_map_pids_to_comms = {}
+        self._buffer_map_pids_to_cmdlines = {}
+        self._buffer_list_of_comms = []
+        self._buffer_map_comms_to_pids = {}
+        self._buffer_map_comms_to_pids = dict()
 
-    def _get_running_pids(self, rebuild_buffer):
-        output = list()
-        if self._buffer_running_pids == [] or rebuild_buffer:
-            output = [pid for pid in _listdir('/proc') if pid.isdigit()]
-            self._buffer_running_pids = output
-        else:
-            output = self._buffer_running_pids
-        return output
+        self._buffer_running_pids = [
+            pid for pid in _listdir('/proc') if pid.isdigit()
+        ]
 
-    def _get_comm_of_pid(self, pid):
-        if pid in self._buffer_map_pids_to_comms:
-            comm = self._buffer_map_pids_to_comms[pid]
-        else:
+        for index, pid in enumerate(self._buffer_running_pids):
             try:
                 comm_file = open(_join('/proc', pid, 'comm'), 'r')
                 comm = comm_file.read().rstrip('\n')
                 comm_file.close()
-            except FileNotFoundError:
-                comm = str()
-        return comm
+                self._buffer_map_pids_to_comms[pid] = comm
 
-    def _get_pids_of_comm(self, comm):
-        try:
+                cmd_file = open(_join('/proc', pid, 'cmdline'), 'r')
+                cmd = cmd_file.read().rstrip('\n')
+                cmd_file.close()
+                cmdline = cmd.split('\x00')
+
+                self._buffer_map_pids_to_cmdlines[pid] = cmdline
+
+                if comm not in self._buffer_list_of_comms:
+                    self._buffer_list_of_comms.append(comm)
+                    self._buffer_map_comms_to_pids[comm] = list()
+
+                self._buffer_map_comms_to_pids[comm].append(pid)
+
+            except FileNotFoundError:
+                self._buffer_running_pids.pop(index)
+
+        self.time_of_init = _ctime()
+
+    def update_snapshot(self):
+        self.__init__()
+        raise DeprecationWarning("Please create a new instance instead.")
+
+    def get_pids(self) -> List[str]:
+        """Returns a list of pids"""
+        return self._buffer_running_pids
+
+    def get_comms(self) -> List[str]:
+        """Returns a list of comms"""
+        return self._buffer_list_of_comms
+
+    def get_comms_to_pids(self) -> Dict[str, List[str]]:
+        """Returns a dict of comms as keys and a list of pids as values"""
+        return self._buffer_map_comms_to_pids
+
+    def get_pids_to_comms(self) -> Dict[str, str]:
+        """Returns a dict of pids as keys and a string of the comm as values"""
+        return self._buffer_map_pids_to_comms
+
+    def get_pids_to_cmdlines(self) -> Dict[str, List[str]]:
+        """Returns a dict of pids as keys and a string of the comm as values"""
+        return self._buffer_map_pids_to_cmdlines
+
+    def get_pids_of_comm(self, comm: str) -> List[str]:
+        """Returns a list of all pids with comm"""
+        if comm in self._buffer_map_comms_to_pids.keys():
             pids = self._buffer_map_comms_to_pids[comm]
-        except KeyError:
+        else:
             pids = []
         return pids
 
-    def _map_pids_to_comms(self, rebuild_buffer):
-        output = dict()
-        if self._buffer_map_pids_to_comms == {} or rebuild_buffer:
-            for i in self._buffer_running_pids:
-                output[i] = self._get_comm_of_pid(i)
-            self._buffer_map_pids_to_comms = output
-        else:
-            output = self._buffer_map_pids_to_comms
-        return output
-
-    def _get_list_of_comms(self, rebuild_buffer):
-        output = list()
-        if self._buffer_list_of_comms == [] or rebuild_buffer:
-            for i in self._buffer_map_pids_to_comms.items():
-                if i[1] not in output:
-                    output.append(i[1])
-            self._buffer_list_of_comms = output
-        else:
-            output = self._buffer_list_of_comms
-        return output
-
-    def _map_comms_to_pids(self, rebuild_buffer):
-        output = dict()
-        if self._buffer_map_comms_to_pids == {} or rebuild_buffer:
-            for i in self._buffer_list_of_comms:
-                output[i] = list()
-            for i in self._buffer_map_pids_to_comms.items():
-                output[i[1]].append(i[0])
-            self._buffer_map_comms_to_pids = output
-        else:
-            output = self._buffer_map_comms_to_pids
-        return output
-
-    def _is_comm_running(self, comm):
-        return comm in self._buffer_list_of_comms
-
-    def _build_buffers(self, rebuild):
-        self._get_running_pids(rebuild)
-        self._map_pids_to_comms(rebuild)
-        self._get_list_of_comms(rebuild)
-        self._map_comms_to_pids(rebuild)
-
-    def update_snapshot(self):
-        self._build_buffers(True)
-
-    def get_pids(self):
-        """Returns a list of pids that were running when this instance
-        was inited"""
-        return self._buffer_running_pids
-
-    def get_comms(self):
-        """Returns a list of comms that were running when this instance
-        was inited"""
-        return self._buffer_list_of_comms
-
-    def get_comms_to_pids(self):
-        """Returns a dict of comms as keys and a list of pids as values that
-        were running when this instance was inited"""
-        return self._buffer_map_comms_to_pids
-
-    def get_pids_to_comms(self):
-        """Returns a dict of pids as keys and a string of the comm as values that
-        were running when this instance was inited"""
-        return self._buffer_map_pids_to_comms
-
-    def get_pids_of_comm(self, comm):
-        """Returns a list of all pids with comm that were running when this
-        instance was inited"""
-        return self._get_pids_of_comm(comm)
-
-    def get_comm_of_pid(self, pid):
+    def get_comm_of_pid(self, pid: str) -> str:
         """Returns the str of the comm of a pid"""
-        return self._get_comm_of_pid(pid)
+        return self._buffer_map_pids_to_comms[pid]
 
-    def is_comm_running(self, comm):
-        """Returns a bool if any process with that comm were running when this
-        instance of the comm was inited"""
-        return self._is_comm_running(comm)
+    def get_cmdline_of_pid(self, pid: str) -> List[str]:
+        """Returns the list with each argv entry of pid as a different string"""
+        return self._buffer_map_pids_to_cmdlines[pid]
 
-
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
+    def is_comm_running(self, comm: str) -> bool:
+        """Returns a bool if any process with that comm is running"""
+        return comm in self._buffer_list_of_comms
