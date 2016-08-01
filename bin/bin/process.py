84046f37f9efec370b08affc82ea32dc511dc22c
Parth Nobel <pnob99@gmail.com>
2016-08-01 00:29:45 -0700

    Complicates _build_buffers in process.py, but reduces it to a single loop

--- bin/bin/process.py
+++ bin/bin/process.py
@@ -12,7 +12,7 @@ __all__ = [
     "get_pids_of_comm",
     "update_buffers",
     "get_cmdline_of_pid",
-    ]
+]
 
 
 from os import listdir as _listdir
@@ -26,71 +26,43 @@ _buffer_running_pids = []  # type: List[str]
 _buffer_list_of_comms = []  # type: List[str]
 
 
-def _map_pids_to_comms(rebuild_buffer: bool) -> Dict[str, str]:
+def _build_buffers() -> None:
     global _buffer_map_pids_to_comms
-    output = dict()
-    if _buffer_map_pids_to_comms == {} or rebuild_buffer:
-        # Maps the contents of _bufer_running_pids to their comms.
-        # The comm getting logic is in _get_comm_of_pid
-        for i in _buffer_running_pids:
-            output[i] = _get_comm_of_pid(i)
-        _buffer_map_pids_to_comms = output
-    else:
-        output = _buffer_map_pids_to_comms
-    return output
-
-
-def _map_pids_to_cmdlines(rebuild_buffer: bool) -> Dict[str, List[str]]:
     global _buffer_map_pids_to_cmdlines
-    output = dict()
-    if _buffer_map_pids_to_cmdlines == {} or rebuild_buffer:
-        # Maps the contents of _bufer_running_pids to their cmdline.
-        # The cmdline getting logic is in _get_cmdline_of_pid
-        for i in _buffer_running_pids:
-            output[i] = _get_cmdline_of_pid(i)
-        _buffer_map_pids_to_cmdlines = output
-    else:
-        output = _buffer_map_pids_to_cmdlines
-    return output
-
-
-def _get_running_pids(rebuild_buffer: bool) -> List[str]:
     global _buffer_running_pids
-    output = list()  # type: List[str]
-    if _buffer_running_pids == [] or rebuild_buffer:
-        output = [pid for pid in _listdir('/proc') if pid.isdigit()]
-        _buffer_running_pids = output
-    else:
-        output = _buffer_running_pids
-    return output
+    global _buffer_list_of_comms
+    global _buffer_map_comms_to_pids
+    global _buffer_map_comms_to_pids
 
+    _buffer_map_pids_to_comms = {}  # Dict[str, str]
+    _buffer_map_pids_to_cmdlines = {}  # Dict[str, List[str]]
+    _buffer_running_pids = [pid for pid in _listdir('/proc') if pid.isdigit()]
+    _buffer_list_of_comms = []  # List[str]
+    _buffer_map_comms_to_pids = {}  # type: Dict[str, List[str]]
+    _buffer_map_comms_to_pids = dict()  # type: Dict[str, List[str]]
 
-def _get_comm_of_pid(pid: str) -> str:
-    if pid in _buffer_map_pids_to_comms:
-        comm = _buffer_map_pids_to_comms[pid]
-    else:
+    for index, pid in enumerate(_buffer_running_pids):
         try:
             comm_file = open(_join('/proc', pid, 'comm'), 'r')
             comm = comm_file.read().rstrip('\n')
             comm_file.close()
-        except FileNotFoundError:
-            comm = str()
-    return comm
+            _buffer_map_pids_to_comms[pid] = comm
 
-
-def _get_cmdline_of_pid(pid: str) -> List[str]:
-    if pid in _buffer_map_pids_to_cmdlines:
-        cmdline = _buffer_map_pids_to_cmdlines[pid]
-    else:
-        try:
             cmd_file = open(_join('/proc', pid, 'cmdline'), 'r')
             cmd = cmd_file.read().rstrip('\n')
             cmd_file.close()
             cmdline = cmd.split('\x00')
+
+            _buffer_map_pids_to_cmdlines[pid] = cmdline
+
+            if comm not in _buffer_list_of_comms:
+                _buffer_list_of_comms.append(comm)
+                _buffer_map_comms_to_pids[comm] = list()
+
+            _buffer_map_comms_to_pids[comm].append(pid)
+
         except FileNotFoundError:
-            cmd = str()
-            cmdline = cmd.split('\x00')
-    return cmdline
+            _buffer_running_pids.pop(index)
 
 
 def _get_pids_of_comm(comm: str) -> List[str]:
@@ -101,47 +73,12 @@ def _get_pids_of_comm(comm: str) -> List[str]:
     return pids
 
 
-def _get_list_of_comms(rebuild_buffer: bool) -> List[str]:
-    global _buffer_list_of_comms
-    output = list()  # type: List[str]
-    if _buffer_list_of_comms == [] or rebuild_buffer:
-        for i in _buffer_map_pids_to_comms.items():
-            if i[1] not in output:
-                output.append(i[1])
-        _buffer_list_of_comms = output
-    else:
-        output = _buffer_list_of_comms
-    return output
-
-
-def _map_comms_to_pids(rebuild_buffer: bool) -> Dict[str, List[str]]:
-    global _buffer_map_comms_to_pids
-    output = dict()  # type: Dict[str, List[str]]
-    if _buffer_map_comms_to_pids == {} or rebuild_buffer:
-        for i in _buffer_list_of_comms:
-            output[i] = list()
-        for j in _buffer_map_pids_to_comms.items():
-            output[j[1]].append(j[0])
-        _buffer_map_comms_to_pids = output
-    else:
-        output = _buffer_map_comms_to_pids
-    return output
-
-
 def _is_comm_running(comm: str) -> bool:
     return comm in _buffer_list_of_comms
 
 
-def _build_buffers(rebuild: bool) -> None:
-    _get_running_pids(rebuild)
-    _map_pids_to_comms(rebuild)
-    _map_pids_to_cmdlines(rebuild)
-    _get_list_of_comms(rebuild)
-    _map_comms_to_pids(rebuild)
-
-
 def update_buffers() -> None:
-    _build_buffers(True)
+    _build_buffers()
 
 
 def get_pids() -> List[str]:
@@ -176,12 +113,12 @@ def get_pids_to_cmdlines() -> Dict[str, List[str]]:
 
 def get_comm_of_pid(pid: str) -> str:
     """Returns the str of the comm of a pid"""
-    return _get_comm_of_pid(pid)
+    return _buffer_map_pids_to_comms[pid]
 
 
 def get_cmdline_of_pid(pid: str) -> List[str]:
     """Returns the list with each argv entry of pid as a different string"""
-    return _get_cmdline_of_pid(pid)
+    return _buffer_map_pids_to_cmdlines[pid]
 
 
 def is_comm_running(comm: str) -> bool:
@@ -189,4 +126,4 @@ def is_comm_running(comm: str) -> bool:
     return _is_comm_running(comm)
 
 
-_build_buffers(False)
+_build_buffers()
